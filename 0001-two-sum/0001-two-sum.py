733bfd74c5a831e320f03889ec84a3e94534a68f
Hogan <purmonth@gmail.com>
2023-07-09 02:00:44 +0800

    Time: 44 ms (81.52%), Space: 14 MB (94.22%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_map = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        
+        return []
