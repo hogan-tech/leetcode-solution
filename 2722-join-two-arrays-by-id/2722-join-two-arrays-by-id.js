7d72654024af95d71221efcdd88257e1ec7792d3
Hogan <purmonth@gmail.com>
2023-07-15 00:11:21 +0800

    Time: 457 ms (16.75%), Space: 121.8 MB (21.04%) - LeetHub

--- 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -3,18 +3,12 @@
  * @param {Array} arr2
  * @return {Array}
  */
-var join = function(arr1, arr2) {
-    arr1 = arr1.concat(arr2)
-    const merge = {}
-    for(item of arr1){
-        const id = item.id
-        if(merge[id]){
-            merge[id] = {...merge[id],...item}
-        }else{
-            merge[id] = {...item}
-        }
-
-    }
-    const resultArray = Object.values(merge)
-    return resultArray
-}
+var join = function (arr1, arr2) {
+  const merge = {};
+  for (item of arr1.concat(arr2)) {
+    merge[item.id] = merge[item.id]
+      ? { ...merge[item.id], ...item }
+      : { ...item };
+  }
+  return Object.values(merge);
+};
