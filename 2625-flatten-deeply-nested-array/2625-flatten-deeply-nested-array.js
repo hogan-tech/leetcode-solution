776629d428c83bdf4369bf37b701c9b62cce8c86
Hogan <purmonth@gmail.com>
2023-07-15 00:37:04 +0800

    Time: 109 ms (80.49%), Space: 61.4 MB (89.86%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -4,5 +4,16 @@
  * @return {any[]}
  */
 var flat = function (arr, n) {
-    return arr.flat(n)
+  const result = [];
+  const flattening = (arr, n) => {
+    for (item of arr) {
+      if (Array.isArray(item) && n > 0) {
+        flattening(item, n - 1);
+      } else {
+        result.push(item);
+      }
+    }
+  };
+  flattening(arr,n)
+  return result;
 };
