7280a24b9f8ad34e1d6bc612999778c873e1889f
Hogan <purmonth@gmail.com>
2023-07-15 13:18:16 +0800

    Time: 3 ms (46.01%), Space: 7 MB (48.38%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,29 @@
+class Solution {
+ public:
+  void rotate(vector<vector<int>>& matrix) {
+    transpose(matrix);
+    reflect(matrix);
+  }
+
+  void transpose(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        int tmp = matrix[j][i];
+        matrix[j][i] = matrix[i][j];
+        matrix[i][j] = tmp;
+      }
+    }
+  }
+
+  void reflect(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n / 2; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[i][n - j - 1];
+        matrix[i][n - j - 1] = tmp;
+      }
+    }
+  }
+};
