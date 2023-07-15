7150471ef02aba43db813465a8578f4101e3a1d4
Hogan <purmonth@gmail.com>
2023-07-15 15:27:14 +0800

    Time: 77 ms (23.49%), Space: 42.2 MB (28.29%) - LeetHub

---
+++ 2715-execute-cancellable-function-with-delay/2715-execute-cancellable-function-with-delay.js
@@ -0,0 +1,39 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  const timer = setTimeout(() => {
+    fn.apply(null,args);
+  }, t);
+  const cancelFn = () => clearTimeout(timer);
+  return cancelFn;
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const start = performance.now() 
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr))
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelT)
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
