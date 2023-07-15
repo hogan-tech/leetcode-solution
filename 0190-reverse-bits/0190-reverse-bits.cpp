ca784c21c814490f4e5cc71972116f19da924a9e
Hogan <purmonth@gmail.com>
2023-07-15 13:39:53 +0800

    Time: 6 ms (33.32%), Space: 6 MB (64.05%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+ public:
+  uint32_t reverseBits(uint32_t n) {
+    uint32_t ret = 0, power = 31;
+    while (n != 0) {
+      ret += (n & 1) << power;
+      n = n >> 1;
+      power -= 1;
+    }
+    return ret;
+  }
+};
