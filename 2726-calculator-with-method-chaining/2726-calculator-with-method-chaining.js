133761a33101e185d0b4311ab9a33e5b81c1f331
Hogan <purmonth@gmail.com>
2023-07-15 14:35:30 +0800

    Time: 57 ms (61.79%), Space: 41.6 MB (69.63%) - LeetHub

---
+++ 2726-calculator-with-method-chaining/2726-calculator-with-method-chaining.js
@@ -0,0 +1,29 @@
+class Calculator {
+  constructor(value) {
+    this.result = value;
+  }
+  add(value){
+    this.result += value;
+    return this;
+  }
+  subtract(value){
+    this.result -= value;
+    return this;
+  }
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+  divide(value) {
+    if(value === 0) throw "Division by zero is not allowed";
+    this.result /= value;
+    return this;
+  }
+  power(value) {
+    this.result **= value;
+    return this;
+  }
+  getResult() {
+    return this.result;
+  }
+}
