2e42d90391287236559b9a7da768b55bca38c244
Hogan <purmonth@gmail.com>
2023-07-15 13:01:18 +0800

    Time: 66 ms (35.51%), Space: 42.4 MB (9.46%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject("Time Limit Exceeded");
+      }, t);
+      fn(...args)
+        .then(resolve)
+        .catch(reject);
+    });
+  };
+};
+
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
