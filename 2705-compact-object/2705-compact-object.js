72ba42a1b103805c3a752d300c0bb60247471415
Hogan <purmonth@gmail.com>
2023-07-15 23:52:44 +0800

    Time: 109 ms (30.05%), Space: 51.9 MB (82.53%) - LeetHub

--- 2705-compact-object/2705-compact-object.js
+++ 2705-compact-object/2705-compact-object.js
@@ -1,33 +1,28 @@
-var compactObject = function(obj) {
+var compactObject = function (obj) {
   function dfs(obj) {
-      if(!obj) return false
-      if(typeof obj !== 'object') return obj
-
-      if(Array.isArray(obj)) {
-          const newArr = []
-          for(let i = 0; i < obj.length; i++) {
-              const curr = obj[i]
-              const subRes = dfs(curr)
-
-              if(subRes) {
-                  newArr.push(subRes)
-              }
-          }
-
-          return newArr
+    if (!obj) return;
+    if (typeof obj !== "object") {
+      return obj;
+    }
+    if (Array.isArray(obj)) {
+      const newArray = [];
+      for (let i = 0; i < obj.length; i++) {
+        const subRes = dfs(obj[i]);
+        if (subRes) newArray.push(subRes);
       }
+      return newArray;
+    }
 
-      const newObj = {}
+    const newObj = {};
 
-      for(const key in obj) {
-          const subRes = dfs(obj[key])
-          if(subRes) {
-              newObj[key] = subRes
-          }
+    for (const key in obj) {
+      const subRes = dfs(obj[key]);
+      if (subRes) {
+        newObj[key] = subRes;
       }
+    }
 
-      return newObj
-  }  
-
-  return dfs(obj)
+    return newObj;
+  }
+  return dfs(obj);
 };
