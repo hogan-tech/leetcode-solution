b13ebf0bf4b297047b0de0c3e1815028decc3444
Hogan <purmonth@gmail.com>
2023-07-17 00:43:59 +0800

    Time: 77 ms (35.78%), Space: 48.3 MB (66.35%) - LeetHub

---
+++ 2632-curry/2632-curry.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function (fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+    return (...nextArgs) => {
+      return curried(...args, ...nextArgs);
+    };
+  };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
