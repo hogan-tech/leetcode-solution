06c522ef78e1cbc57f91bbf8e81ba6b7c249fa1e
Hogan <purmonth@gmail.com>
2023-07-16 22:37:45 +0800

    Time: 217 ms (5.05%), Space: 59.9 MB (5.04%) - LeetHub

--- 2628-json-deep-equal/2628-json-deep-equal.js
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -3,21 +3,14 @@
  * @param {any} o2
  * @return {boolean}
  */
-var areDeeplyEqual = function (o1, o2) {
-  if (o1 === o2) return true;
-  if (o1 === null || o2 === null) return false;
-  if (String(o1) !== String(o2)) return false;
-  if (typeof o1 !== "object") return o1 === o2;
-  if (Array.isArray(o1)) {
-    if (o1.length !== o2.length) return false;
-    for (let i = 0; i < o1.length; i++) {
-      if (!areDeeplyEqual(o1[i], o2[i])) return false;
-    }
-    return true;
-  }
-  if (Object.keys(o1).length !== Object.keys(o2).length) return false;
-  for (const key in o1) {
-    if (!areDeeplyEqual(o1[key], o2[key])) return false;
-  }
-  return true;
+const helper = (key,value) => {
+    return (value && !Array.isArray(value) && typeof value === "object")
+    ? Object.fromEntries(Object.entries(value).sort())
+    : value;
+};
+
+let areDeeplyEqual = function (o1, o2) {
+    console.log(JSON.stringify(o1, helper))
+    console.log(JSON.stringify(o2, helper))
+  return JSON.stringify(o1, helper) === JSON.stringify(o2, helper);
 };
