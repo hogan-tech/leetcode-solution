f9308f93bfbbcf55fac0e334d08d0bcd33c19b5c
Hogan <purmonth@gmail.com>
2023-07-16 23:50:06 +0800

    Time: 92 ms (7.95%), Space: 43 MB (8.93%) - LeetHub

--- 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -4,23 +4,19 @@
  */
 var promiseAll = async function (functions) {
   return new Promise((resolve, reject) => {
-    if (functions.length === 0) {
+    if (functions.length == 0) {
       resolve([]);
       return;
     }
 
-    const res = new Array(functions.length).fill(null);
-
+    const res = Array(functions.length).fill(null);
     let resolvedCount = 0;
-
-    functions.forEach(async (element, idx) => {
+    functions.forEach(async (item, idx) => {
       try {
-        const subRes = await element();
+        const subRes = await item();
         res[idx] = subRes;
         resolvedCount++;
-        if (resolvedCount === functions.length) {
-          return resolve(res);
-        }
+        if (resolvedCount === functions.length) return resolve(res);
       } catch (err) {
         reject(err);
       }
