f9d1a66dab24c4740547521407e35db1fe02a2bb
Hogan <purmonth@gmail.com>
2023-07-20 10:16:29 +0800

    Time: 47 ms (50.94%), Space: 16.4 MB (31.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,26 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        memo = [0] * (n+1)
-        return self.climb_Stairs(0, n, memo)
+#     def climbStairs(self, n: int) -> int:
+#         memo = [0] * (n+1)
+#         return self.climb_Stairs(0, n, memo)
 
-    def climb_Stairs(self, i: int, n: int, memo: list) -> int:
-        if (i > n):
-            return 0
-        if (i == n):
+#     def climb_Stairs(self, i: int, n: int, memo: list) -> int:
+#         if (i > n):
+#             return 0
+#         if (i == n):
+#             return 1
+#         if (memo[i]):
+#             return memo[i]
+#         memo[i] = self.climb_Stairs(i+1, n, memo) + \
+#             self.climb_Stairs(i+2, n, memo)
+#         return memo[i]
+
+    def climbStairs(self, n: int):
+        if n == 1:
             return 1
-        if (memo[i]):
-            return memo[i]
-        memo[i] = self.climb_Stairs(i+1, n, memo) + \
-            self.climb_Stairs(i+2, n, memo)
-        return memo[i]
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
