d370153e3c61846d864eaf21f69bb1e3428c5943
Hogan <purmonth@gmail.com>
2023-07-23 15:59:19 +0800

    Time: 639 ms (5.24%), Space: 16.5 MB (67.04%) - LeetHub

--- 0439-ternary-expression-parser/0439-ternary-expression-parser.py
+++ 0439-ternary-expression-parser/0439-ternary-expression-parser.py
@@ -1,21 +1,12 @@
 class Solution:
     def parseTernary(self, expression: str) -> str:
         if not expression:
-            return ""
-        validString = 'TF0123456789'
+            return ''
+
+        validChars = 'TF0123456789'
 
         def isValidAtomic(s: str):
-            if s[0] not in 'TF':
-                return False
-            if s[1] != '?':
-                return False
-            if s[2] not in validString:
-                return False
-            if s[3] != ':':
-                return False
-            if s[4] not in validString:
-                return False
-            return True
+            return len(s) >= 5 and s[0] in 'TF' and s[1] == '?' and s[2] in validChars and s[3] == ':' and s[4] in validChars
 
         def solveAtomic(s: str):
             return s[2] if s[0] == 'T' else s[4]
@@ -28,3 +19,7 @@ class Solution:
                 solveAtomic(expression[j-4:j+1]) + expression[j+1:]
 
         return expression
+
+
+Expression = 'F?1:T?4:5'
+
