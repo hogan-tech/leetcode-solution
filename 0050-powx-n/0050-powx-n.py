0f2b28733cc20ab323c8278d88a686b5981b906c
Hogan <purmonth@gmail.com>
2023-07-24 10:20:40 +0800

    Time: 41 ms (85.77%), Space: 16.4 MB (7.14%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,28 @@
 class Solution:
-    def binaryExp(self, x: float, n: int) -> float:
+    def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
-       
         if n < 0:
-            return 1.0 / self.binaryExp(x, -1 * n)
-       
-        if n % 2 == 1:
-            return x * self.binaryExp(x * x, (n - 1) // 2)
+            if abs(x) < 1e-6:
+                raise ValueError("Cannot calculate power for x close to zero with negative n")
+            result = 1.0
+            x = 1 / x
+            n = -n
+            while n > 0:
+                if n % 2 == 1:
+                    result *= x
+                x *= x
+                n //= 2
+            return result
         else:
-            return self.binaryExp(x * x, n // 2)
+            result = 1.0
+            while n > 0:
+                if n % 2 == 1:
+                    result *= x
+                x *= x
+                n //= 2
+            return result
 
-    def myPow(self, x: float, n: int) -> float:
-        return self.binaryExp(x, n)
+
+x = 2.00000
+n = -10
