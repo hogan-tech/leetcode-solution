35089c53331b8fb999f5ce1f0d786e2b8d7ba992
Hogan <purmonth@gmail.com>
2023-07-26 00:28:40 +0800

    Time: 1334 ms (67.08%), Space: 37.1 MB (17.17%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        @lru_cache(None)
+        def dfs(amount):
+            if amount < 0:
+                return -1
+            if amount == 0:
+                return 0
+            min_cost = float('inf')
+            for coin in coins:
+                res = dfs(amount - coin)
+                if res != -1:
+                    min_cost = min(min_cost, res + 1)
+            return -1 if min_cost == float('inf') else min_cost
+
+        return dfs(amount)
