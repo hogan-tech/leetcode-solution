33243178ac3fa6f373568860adb10beaf74f2929
Hogan <purmonth@gmail.com>
2023-07-28 00:59:04 +0800

    Time: 37 ms (99.79%), Space: 16.6 MB (41.76%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -8,3 +8,16 @@ class Solution:
         for i, item in enumerate(list(permutations(nums))):
             result.append(list(item))
         return result
+
+        # out = []
+        # stack = [[]]
+        # while stack:
+        #     curr = stack.pop()
+        #     if len(curr) == len(nums):
+        #         out.append(curr)
+        #         continue
+        #     for num in nums:
+        #         if num not in curr:
+        #             stack.append((curr + [num]))
+        #     print(out)
+        # return []
