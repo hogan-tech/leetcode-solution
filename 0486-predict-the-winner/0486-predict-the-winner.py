7e4becb7909475ca1ec59cb293195da3e90ea458
Hogan <purmonth@gmail.com>
2023-07-30 00:06:26 +0800

    Time: 35 ms (99.39%), Space: 16.6 MB (31.88%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -4,14 +4,10 @@ from typing import List
 
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        n = len(nums)
-
         @lru_cache(None)
         def maxDiff(left, right):
             if left == right:
                 return nums[left]
-            score_by_left = nums[left] - maxDiff(left + 1, right)
-            score_by_right = nums[right] - maxDiff(left, right - 1)
-            return max(score_by_left, score_by_right)
+            return max(nums[left] - maxDiff(left + 1, right), nums[right] - maxDiff(left, right - 1))
 
-        return maxDiff(0, n-1) >= 0
+        return maxDiff(0, len(nums)-1) >= 0
