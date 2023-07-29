8f6bb41705a28a52e077d4b219076118fba6b117
Hogan <purmonth@gmail.com>
2023-07-30 01:32:52 +0800

    Time: 190 ms (24.06%), Space: 45.8 MB (6.13%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -1,12 +1,12 @@
-import collections
+from functools import lru_cache
 from math import ceil
 
 
 class Solution:
     def soupServings(self, n: int) -> float:
         m = ceil(n/25)
-        dp = collections.defaultdict(dict)
 
+        @lru_cache(None)
         def calculate_dp(i: int, j: int) -> float:
             if i <= 0 and j <= 0:
                 return 0.5
@@ -14,11 +14,8 @@ class Solution:
                 return 1.0
             if j <= 0:
                 return 0.0
-            if i in dp and j in dp[i]:
-                return dp[i][j]
-            dp[i][j] = (calculate_dp(i-4, j) + calculate_dp(i-3, j-1) +
-                        calculate_dp(i-2, j-2) + calculate_dp(i-1, j-3)) / 4.0
-            return dp[i][j]
+            return (calculate_dp(i-4, j) + calculate_dp(i-3, j-1) +
+                    calculate_dp(i-2, j-2) + calculate_dp(i-1, j-3)) / 4.0
 
         for k in range(1, m + 1):
             if calculate_dp(k, k) > 1 - 1e-5:
