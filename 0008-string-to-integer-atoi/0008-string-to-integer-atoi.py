8bdd1a1f7db65d6a5860c6bb70086a76e020090c
Hogan <purmonth@gmail.com>
2023-08-01 21:26:19 +0800

    Time: 39 ms (96.48%), Space: 16.2 MB (76.44%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,54 +1,35 @@
-import numpy as np
-
-
 class Solution:
     def myAtoi(self, s: str) -> int:
-        n = len(s)
-        if n == 0:
-            return 0
-        c = ' '
-        i = 0
-        while c == ' ' and i < n:
-            c = s[i]
-            i += 1
-        i -= 1
-
-        sgn = 1
-        x = 0
-        digit_len = 0
-        c = s[i]
-        if c == '-':
-            sgn = -1
-        elif c == '+':
-            sgn = 1
-        elif not np.char.isdigit(c):
-            return 0
-        else:
-            x = ord(c)-ord('0')
-            if x > 0:
-                digit_len += 1
-        i += 1
-
-        while i < n and digit_len <= 12:
-            c = s[i]
-            if not np.char.isdigit(c):
+        x = ""
+        y = ""
+        start = 0
+        nums = "0123456789"
+        for i in range(len(s)):
+            if s[i] != " ":
+                start = i
                 break
-            x = 10*x+ord(c)-ord('0')
-            i += 1
-            if x > 0:
-                digit_len += 1
-        x = sgn*x
+        for i in range(start, len(s)):
+            if s[i] in "-+":
+                if x == "" and y == "":
+                    x = s[i]
+                else:
+                    break
+            elif s[i] in nums:
+                y += s[i]
+            else:
+                break
 
-        INT_MAX = 2**31-1
-        INT_MIN = -2**31
-        if x > INT_MAX:
-            x = INT_MAX
-        elif x < INT_MIN:
-            x = INT_MIN
-        return x
+        if y == "":
+            return 0
+        if x == "-":
+            if int(y) > 2**31:
+                return -2**31
+            else:
+                return -int(y)
+        else:
+            if int(y) > 2**31-1:
+                return 2**31-1
+            else:
+                return int(y)
 
 
-S = "   -42"
-
-
-print(Solution().myAtoi(S))
