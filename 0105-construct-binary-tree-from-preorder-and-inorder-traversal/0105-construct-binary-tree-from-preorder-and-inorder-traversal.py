c0d1763bf31c21bffaf714b4f9802b1981d88014
Hogan <purmonth@gmail.com>
2023-08-03 02:08:10 +0800

    Time: 56 ms (99.27%), Space: 21.2 MB (83.23%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -12,21 +12,20 @@ class TreeNode:
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
 
-        def array_to_tree(left, right):
+        def array_to_tree(left: int, right: int) -> Optional[TreeNode]:
             nonlocal preorder_index
             if left > right:
                 return None
             root_value = preorder[preorder_index]
             root = TreeNode(root_value)
-
             preorder_index += 1
-
-            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
-            root.right = array_to_tree(inorder_index_map[root_value]+1, right)
+            root.left = array_to_tree(left, inorder_map[root_value] - 1)
+            root.right = array_to_tree(inorder_map[root_value] + 1, right)
             return root
 
         preorder_index = 0
-        inorder_index_map = {}
+        inorder_map = {}
         for index, value in enumerate(inorder):
-            inorder_index_map[value] = index
-        return array_to_tree(0, len(preorder) - 1)
+            inorder_map[value] = index
+
+        return array_to_tree(0, len(inorder) - 1)
