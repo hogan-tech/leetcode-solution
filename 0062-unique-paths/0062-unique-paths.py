c41d067e3fa87154d77faab794ae2f0184462d65
Hogan <purmonth@gmail.com>
2023-08-03 16:04:01 +0800

    Time: 45 ms (66.50%), Space: 17.1 MB (7.39%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,9 @@
+from functools import lru_cache
+
+
+class Solution:
+    @lru_cache(None)
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
