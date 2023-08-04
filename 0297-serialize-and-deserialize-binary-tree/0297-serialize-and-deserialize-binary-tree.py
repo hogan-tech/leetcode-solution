02cabff1c77896bb893fb9e57b7be8cbf3325287
Hogan <purmonth@gmail.com>
2023-08-04 22:42:17 +0800

    Time: 107 ms (94.09%), Space: 22.3 MB (73.35%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -1,53 +1,40 @@
-# Definition for a binary tree node.
-import collections
-from typing import List
-
-
-
 class Codec:
-
-
-    def serialize(self, root):
+    def serialize(self, root: Optional[TreeNode]) -> str:
         if not root:
             return ''
-        queue = collections.deque()
-        queue.append(root)
-        res = ''
+
+        queue = deque([root])
+        res = []
         while queue:
             node = queue.popleft()
-            if not node:
-                res += 'None,'
-                continue
-            res += str(node.val) + ','
-            queue.append(node.left)
-            queue.append(node.right)
-        return res
+            if node:
+                res.append(str(node.val))
+                queue.extend([node.left, node.right])
+            else:
+                res.append('None')
+        return ','.join(res)
 
-    def deserialize(self, data):
+    def deserialize(self, data: str) -> Optional[TreeNode]:
         if not data:
             return None
+
         ls = data.split(',')
         root = TreeNode(int(ls[0]))
-        queue = collections.deque()
-        queue.append(root)
+        queue = deque([root])
+
         i = 1
         while queue and i < len(ls):
             node = queue.popleft()
-            if ls[i] != 'None':
-                left = TreeNode(int(ls[i]))
-                node.left = left
-                queue.append(left)
-            i += 1
-            if ls[i] != 'None':
-                right = TreeNode(int(ls[i]))
-                node.right = right
-                queue.append(right)
-            i += 1
+            left_val, right_val = ls[i], ls[i+1] if i + 1 < len(ls) else 'None'
+
+            if left_val != 'None':
+                node.left = TreeNode(int(left_val))
+                queue.append(node.left)
+
+            if right_val != 'None':
+                node.right = TreeNode(int(right_val))
+                queue.append(node.right)
+
+            i += 2
+
         return root
-
-
-root = TreeNode(0)
-# Your Codec object will be instantiated and called as such:
-ser = Codec()
-deser = Codec()
-ans = deser.deserialize(ser.serialize(root))
