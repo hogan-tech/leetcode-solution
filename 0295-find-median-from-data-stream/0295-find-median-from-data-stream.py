d20a8729bbfda245d146f563f06599d8d01f3b29
Hogan <purmonth@gmail.com>
2023-08-05 12:26:42 +0800

    Time: 380 ms (99.68%), Space: 38 MB (82.96%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.heaps = [], []
+
+    def addNum(self, num: int) -> None:
+        small, large = self.heaps
+        heapq.heappush(small, -heapq.heappushpop(large, num))
+        if len(large) < len(small):
+            heapq.heappush(large, -heapq.heappop(small))
+
+    def findMedian(self) -> float:
+        small, large = self.heaps
+        if len(large) > len(small):
+            return float(large[0])
+        return (large[0] - small[0]) / 2.0
