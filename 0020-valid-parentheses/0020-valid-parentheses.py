958e630bdbda577248f4cc932b45a380ef06f7f1
Hogan <purmonth@gmail.com>
2023-08-08 01:50:33 +0800

    Time: 39 ms (86.80%), Space: 16.3 MB (54.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,5 +1,13 @@
-class Solution(object):
-    def isValid(self, s):
-        while "()" in s or "{}" in s or '[]' in s:
-            s = s.replace("()", "").replace('{}', "").replace('[]', "")
-        return s == ''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracketMap = {"(": ")", "[": "]", "{": "}"}
+        openSet = set(["(", "[", "{"])
+        stack = []
+        for char in s:
+            if char in openSet:
+                stack.append(char)
+            elif stack and char == bracketMap[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
