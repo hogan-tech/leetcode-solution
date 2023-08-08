0493d90a28953d55790df219075e446161eb7d7c
Hogan <purmonth@gmail.com>
2023-08-08 23:44:06 +0800

    Time: 36 ms (95.98%), Space: 16.4 MB (99.20%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtracking(curString: str, leftCount: int, rightCount: int) -> None:
+            if len(curString) == 2 * n:
+                ans.append(curString)
+                return
+            if leftCount < n:
+                backtracking(curString + "(", leftCount + 1, rightCount)
+            if rightCount < leftCount:
+                backtracking(curString + ")", leftCount, rightCount + 1)
+
+        ans = []
+        backtracking("", 0, 0)
+        return ans
