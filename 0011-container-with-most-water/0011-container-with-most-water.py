a31f35bc1580fff7d591c7363a887583d06a907e
Hogan <purmonth@gmail.com>
2023-08-16 00:57:55 +0800

    Time: 607 ms (78.16%), Space: 29.5 MB (13.11%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,14 +3,19 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        MaxSum = 0
+        maxSum = 0
+        left, right = 0, len(height) - 1
         while left < right:
-            MaxSum = max(MaxSum, (right - left) *
+            maxSum = max(maxSum, (right - left) *
                          min(height[left], height[right]))
-            if height[left] <= height[right]:
+            if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-        return MaxSum
+
+        return maxSum
+
+
+Height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+print(Solution().maxArea(Height))
