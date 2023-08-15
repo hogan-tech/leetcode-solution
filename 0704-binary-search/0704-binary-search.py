ba23d7b7abc160c61a0fa605a77ec17dd6fef4c1
Hogan <purmonth@gmail.com>
2023-08-16 01:12:52 +0800

    Time: 202 ms (98.95%), Space: 18 MB (32.03%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,13 +1,18 @@
 class Solution:
     def search(self, nums, target):
-        left = 0
-        right = len(nums)-1
+        left, right = 0, len(nums) - 1
         while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[mid] < target:
-                left = mid+1
+            mid = left + (right - left) // 2
             if nums[mid] > target:
-                right = mid-1
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
         return -1
+
+
+numbers = [5]
+target = 5
+solution = Solution()
+print(solution.search(numbers, target))
