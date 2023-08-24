0546a04fb71fa0d6f7522199df572e37d7cd0107
Hogan <purmonth@gmail.com>
2023-08-24 23:47:22 +0800

    Time: 670 ms (76.31%), Space: 74.7 MB (33.40%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,4 +1,4 @@
-from sortedcontainers import SortedDict
+from typing import List
 
 
 class TimeMap:
@@ -8,15 +8,32 @@ class TimeMap:
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if not key in self.map:
-            self.map[key] = SortedDict()
-        self.map[key][timestamp] = value
+            self.map[key]: List = []
+        self.map[key].append([timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
         if not key in self.map:
             return ""
-        
-        it = self.map[key].bisect_right(timestamp)
-        if it == 0:
+
+        if timestamp < self.map[key][0][0]:
             return ""
 
-        return self.map[key].peekitem(it - 1)[1]
+        left, right = 0, len(self.map[key])
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if self.map[key][mid][0] <= timestamp:
+                left = mid + 1
+            else:
+                right = mid
+        return "" if right == 0 else self.map[key][right-1][1]
+
+
+# Your TimeMap object will be instantiated and called as such:
+timeMap = TimeMap()
+timeMap.set("foo", "bar", 1)
+print(timeMap.get("foo", 1))
+print(timeMap.get("foo", 3))
+timeMap.set("foo", "bar2", 4)
+print(timeMap.get("foo", 4))
+print(timeMap.get("foo", 5))
