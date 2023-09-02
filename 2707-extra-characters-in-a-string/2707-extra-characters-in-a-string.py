4da6efa0b5308dad14b07538582ddf38b0a63e76
Hogan <purmonth@gmail.com>
2023-09-03 00:00:54 +0800

    Time: 231 ms (77.25%), Space: 16.8 MB (41.01%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        n, dictionarySet = len(s), set(dictionary)
+        @lru_cache(None)
+        def dp(start):
+            if start == n:
+                return 0
+            ans = dp(start + 1) + 1
+            for end in range(start, n):
+                curr = s[start: end+1]
+                if curr in dictionarySet:
+                    ans = min(ans, dp(end+1))
+            return ans
+        return dp(0)
+
+
+s = "leetscode"
+dictionary = ["leet", "code", "leetcode"]
+
+print(Solution().minExtraChar(s, dictionary))
