d6cf4c169d8f3a8249cbfcbc690fe499b293e29d
Hogan <purmonth@gmail.com>
2023-09-10 16:39:28 +0800

    Time: 93 ms (93.05%), Space: 62.9 MB (87.05%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.ts
@@ -0,0 +1,25 @@
+type MultiDimensionalArray = (number | MultiDimensionalArray)[];
+
+var flat = function (
+    arr: MultiDimensionalArray,
+    n: number
+): MultiDimensionalArray {
+    if (n === 0) return arr;
+    let result: MultiDimensionalArray = [];
+    const flatting = (array: MultiDimensionalArray, left: number) => {
+        for (const item of array) {
+            if (Array.isArray(item) && left > 0) {
+                flatting(item, left - 1);
+            } else {
+                result.push(item);
+            }
+        }
+    };
+    flatting(arr, n);
+    return result;
+};
+
+const array = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+const n = 1;
+
+console.log(flat(array, n));
