33b3705ed51f2c508570a5d2e005a5e7cc395bc5
Hogan <purmonth@gmail.com>
2023-09-11 00:40:54 +0800

    Time: 117 ms (13.79%), Space: 80.5 MB (6.90%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.ts
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.ts
@@ -1,3 +1,27 @@
 function jsonStringify(object: any): string {
-	return JSON.stringify(object)
-};
+    switch (typeof object) {
+        case "object":
+            if (Array.isArray(object)) {
+                const elements = object.map((element) =>
+                    jsonStringify(element)
+                );
+                return `[${elements.join(",")}]`;
+            } else if (object) {
+                const keys = Object.keys(object);
+                const keyValuePairs = keys.map((key) => {
+                    return `"${key}":${jsonStringify(object[key])}`;
+                });
+                return `{${keyValuePairs.join(",")}}`;
+            } else {
+                return "null";
+            }
+            return object;
+        case "boolean":
+        case "number":
+            return `${object}`;
+        case "string":
+            return `"${object}"`;
+        default:
+            return "";
+    }
+}
