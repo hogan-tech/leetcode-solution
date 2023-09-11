f29245b7e7f3407b58da03f7d9761cf88e0a9584
Hogan <purmonth@gmail.com>
2023-09-11 22:02:23 +0800

    Time: 79 ms (11.21%), Space: 42.6 MB (87.07%) - LeetHub

---
+++ 2636-promise-pool/2636-promise-pool.ts
@@ -0,0 +1,30 @@
+type F = () => Promise<any>;
+
+function promisePool(functions: F[], n: number): Promise<any> {
+    return new Promise((resolve, reject) => {
+        let inProgressCount = 0;
+        let functionIndex = 0;
+        function helper() {
+            if (functionIndex >= functions.length) {
+                if (inProgressCount === 0) resolve(0);
+                return;
+            }
+
+            while (inProgressCount < n && functionIndex < functions.length) {
+                inProgressCount++;
+                const promise = functions[functionIndex]();
+                functionIndex++;
+                promise.then(() => {
+                    inProgressCount--;
+                    helper();
+                });
+            }
+        }
+        helper();
+    });
+}
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
