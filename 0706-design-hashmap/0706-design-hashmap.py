a5ef65546a6376f162290dc5b3bdc8c9a26c141f
Hogan <purmonth@gmail.com>
2023-10-16 00:22:25 -0500

    Time: 220 ms (56.96%), Space: 19.9 MB (61.52%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -0,0 +1,51 @@
+class ListNode:
+    def __init__(self, key=-1, val=-1, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+
+class MyHashMap:
+    def __init__(self):
+        self.map = [ListNode() for i in range(1000)]
+
+    def hash(self, key):
+        return key % len(self.map)
+
+    def put(self, key: int, value: int) -> None:
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.val = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        cur = self.map[self.hash(key)].next
+        while cur:
+            if cur.key == key:
+                return cur.val
+            cur = cur.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        cur = self.map[self.hash(key)]
+        while cur and cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
+
+
+# Your MyHashMap object will be instantiated and called as such:
+obj = MyHashMap()
+obj.put(1, 1)
+obj.put(2, 2)
+print(obj.get(1))
+print(obj.get(2))
+print(obj.get(3))
+obj.put(2, 1)
+print(obj.get(2))
+obj.remove(2)
+print(obj.get(2))
