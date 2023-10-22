f0cb3c5ed2101982bcbf9a3e8c0ff48bd8e105fc
Hogan <purmonth@gmail.com>
2023-10-22 12:55:37 +0800

    Time: 43 ms (75.25%), Space: 16.6 MB (60.60%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mappingST,mappingTS = {},{}
+        for c1, c2 in zip(s,t):
+            if (c1 not in mappingST) and (c2 not in mappingTS):
+                mappingST[c1] = c2
+                mappingTS[c2] = c1
+            elif mappingST.get(c1) != c2 or mappingTS.get(c2) != c1:
+                return False
+        return True
+
+
+s = "egf"
+t = "add"
+
+print(Solution().isIsomorphic(s, t))
