04c52b6feb8a7c13823c95695fcbadd55f741bf2
Hogan <purmonth@gmail.com>
2023-10-26 13:25:21 +0800

    Time: 281 ms (83.33%), Space: 16.5 MB (68.75%) - LeetHub

---
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution(object):
+    def numFactoredBinaryTrees(self, A):
+        MOD = 10 ** 9 + 7
+        N = len(A)
+        A.sort()
+        dp = [1] * N
+        index = {x: i for i, x in enumerate(A)}
+        for i, x in enumerate(A):
+            for j in range(i):
+                if x % A[j] == 0:  # A[j] will be left child
+                    right = x / A[j]
+                    if right in index:
+                        dp[i] += dp[j] * dp[index[right]]
+                        dp[i] %= MOD
+
+        return sum(dp) % MOD
+    
+
+print(Solution().numFactoredBinaryTrees([2,4]))
