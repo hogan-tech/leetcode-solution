ede877102ef32564de8fcf418713a2b1713396b7
Hogan <purmonth@gmail.com>
2023-12-21 23:33:19 +0800

    Time: 1231 ms (69.48%), Space: 60.9 MB (5.21%) - LeetHub

---
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -0,0 +1,21 @@
+from functools import cache
+
+
+class Solution:
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+        @cache
+        def dp(i, max_so_far, remain):
+            if i == n:
+                if remain == 0:
+                    return 1
+
+                return 0
+
+            ans = (max_so_far * dp(i + 1, max_so_far, remain)) % MOD
+            for num in range(max_so_far + 1, m + 1):
+                ans = (ans + dp(i + 1, num, remain - 1)) % MOD
+
+            return ans
+
+        MOD = 10 ** 9 + 7
+        return dp(0, 0, k)
