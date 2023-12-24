43cee9027649eafc5b7e94ff1e59c94dce9f7c25
Hogan <purmonth@gmail.com>
2023-12-24 12:47:14 +0800

    Time: 45 ms (85.50%), Space: 17.5 MB (5.67%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        startZero = 0
+        startOne = 0
+        for i in range(len(s)):
+            if i % 2:
+                if s[i] == '1':
+                    startOne += 1
+                else:
+                    startZero += 1
+            else:
+                if s[i] == '0':
+                    startOne += 1
+                else:
+                    startZero += 1
+                    
+        return min(startOne, startZero)
+
+
+s = "1111"
+
+print(Solution().minOperations(s))
