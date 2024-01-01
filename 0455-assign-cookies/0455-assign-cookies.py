e7eddcb3ec1d08f78bbdff633921562812eb44d8
Hogan <purmonth@gmail.com>
2024-01-01 14:54:02 +0800

    Time: 176 ms (13.69%), Space: 19.3 MB (8.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,17 +1,19 @@
+# time complexity: O(nlogn + mlogm)
+# space complexity: O(m + n)
 from typing import List
 
 
 class Solution:
-	def findContentChildren(self, greedyChildren: List[int], cookiesSize: List[int]) -> int:
-		greedyChildren.sort()
-		cookiesSize.sort()
-		cookieIdx = 0
-		childrenIdx = 0
-		while cookieIdx < len(cookiesSize) and childrenIdx < len(greedyChildren):
-			if cookiesSize[cookieIdx] >= greedyChildren[childrenIdx]:
-				childrenIdx += 1
-			cookieIdx += 1
-		return childrenIdx
+    def findContentChildren(self, greedyChildren: List[int], cookiesSize: List[int]) -> int:
+        greedyChildren.sort()
+        cookiesSize.sort()
+        cookieIdx = 0
+        childrenIdx = 0
+        while cookieIdx < len(cookiesSize) and childrenIdx < len(greedyChildren):
+            if cookiesSize[cookieIdx] >= greedyChildren[childrenIdx]:
+                childrenIdx += 1
+            cookieIdx += 1
+        return childrenIdx
 
 
 greedyChildren = [1, 2, 3]
