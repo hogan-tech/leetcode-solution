5cf6fe6f729942f88c765654cd648c11bd710826
Hogan <purmonth@gmail.com>
2024-01-16 21:51:41 +0800

    Time: 48 ms (62.53%), Space: 17.2 MB (43.56%) - LeetHub

---
+++ 1165-single-row-keyboard/1165-single-row-keyboard.py
@@ -0,0 +1,17 @@
+class Solution:
+	def calculateTime(self, keyboard: str, word: str) -> int:
+		count = 0
+		keyMap = {}
+		prevIdx = 0
+		for i in range(len(keyboard)):
+			keyMap[keyboard[i]] = i
+		for i in range(len(word)):
+			count += abs(keyMap[word[i]] - prevIdx)
+			prevIdx = keyMap[word[i]]
+		return count
+
+
+keyboard = "pqrstuvwxyzabcdefghijklmno"
+word = "leetcode"
+
+print(Solution().calculateTime(keyboard, word))
