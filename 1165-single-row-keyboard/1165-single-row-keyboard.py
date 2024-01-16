eadf5ee1d8f65e327235bebeb714c3b444446c05
Hogan <purmonth@gmail.com>
2024-01-16 21:52:17 +0800

    Time: 51 ms (49.41%), Space: 17.3 MB (30.44%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.py
+++ 1165-single-row-keyboard/1165-single-row-keyboard.py
@@ -1,14 +1,16 @@
+# time complexity: O(n)
+# space complexity: O(1)
 class Solution:
-	def calculateTime(self, keyboard: str, word: str) -> int:
-		count = 0
-		keyMap = {}
-		prevIdx = 0
-		for i in range(len(keyboard)):
-			keyMap[keyboard[i]] = i
-		for i in range(len(word)):
-			count += abs(keyMap[word[i]] - prevIdx)
-			prevIdx = keyMap[word[i]]
-		return count
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        count = 0
+        keyMap = {}
+        prevIdx = 0
+        for i in range(len(keyboard)):
+            keyMap[keyboard[i]] = i
+        for i in range(len(word)):
+            count += abs(keyMap[word[i]] - prevIdx)
+            prevIdx = keyMap[word[i]]
+        return count
 
 
 keyboard = "pqrstuvwxyzabcdefghijklmno"
