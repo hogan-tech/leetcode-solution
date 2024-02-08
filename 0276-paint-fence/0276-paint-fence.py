e3294626bee24d18c639dbc3a41d9d861ab2a8ef
Hogan <purmonth@gmail.com>
2024-02-08 12:20:55 +0800

    Time: 31 ms (85.11%), Space: 16.6 MB (59.56%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -1,15 +1,15 @@
+from functools import lru_cache
+
+
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        memo = {}
+        @lru_cache(None)
         def totalWays(i):
             if i == 1:
                 return k
             if i == 2:
                 return k * k
-            if i in memo:
-                return memo[i]
-            memo[i] = (k-1) * (totalWays(i-1) + totalWays(i-2))
-            return memo[i]
+            return (k-1) * (totalWays(i-1) + totalWays(i-2))
 
         return totalWays(n)
 
