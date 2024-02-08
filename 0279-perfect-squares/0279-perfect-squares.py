7ac40cfbb316c22a22a4ba3bfa567a7813f4d4ef
Hogan <purmonth@gmail.com>
2024-02-08 12:05:56 +0800

    Time: 2194 ms (70.32%), Space: 16.5 MB (88.79%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,18 @@
+import math
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squareNums = [i ** 2 for i in range(0, int(math.sqrt(n)) + 1)]
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        for i in range(1, n + 1):
+            for square in squareNums:
+                if i < square:
+                    break
+                dp[i] = min(dp[i], dp[i-square] + 1)
+        return dp[-1]
+
+
+n = 13
+print(Solution().numSquares(n))
