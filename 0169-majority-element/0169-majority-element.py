e8dd729659ad70bd39aa49f19b90982d8e1bcfc3
Hogan <purmonth@gmail.com>
2024-02-12 10:00:24 +0800

    update

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,14 @@
+# time complexity: O(n)
+# space complexity: O(n)
+from collections import Counter
+from typing import List
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        numsSet = Counter(nums)
+        return max(numsSet.keys(), key=numsSet.get)
+
+
+Nums = [2, 2, 1, 1, 1, 2, 2]
+print(Solution().majorityElement(Nums))
