adfe718d453fb16bb46b93e86a663e283099eafb
Hogan <purmonth@gmail.com>
2024-02-19 20:02:59 +0800

    Time: 38 ms (49.49%), Space: 16.6 MB (53.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,3 +1,5 @@
+# time complexity: O(logn)
+# space complexity: O(1)
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n == 0:
@@ -5,5 +7,7 @@ class Solution:
         while n % 2 == 0:
             n //= 2
         return n == 1
+
+
 n = 16
 print(Solution().isPowerOfTwo(n))
