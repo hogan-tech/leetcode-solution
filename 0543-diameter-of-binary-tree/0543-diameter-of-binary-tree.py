052b2c64aa667c5fd56382b9a37f226e67a510ff
Hogan <purmonth@gmail.com>
2024-02-27 23:05:46 +0800

    update readme

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -1,22 +1,34 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# time complexity: O(n)
+# space complexity: O(n)
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         diameter = 0
 
-        def longest_path(node: Optional[TreeNode]):
+        def longestPath(node: Optional[TreeNode]):
             if not node:
                 return 0
             nonlocal diameter
-            left_path = longest_path(node.left)
-            right_path = longest_path(node.right)
+            leftPath = longestPath(node.left)
+            rightPath = longestPath(node.right)
+            diameter = max(diameter, leftPath + rightPath)
+            return max(leftPath, rightPath) + 1
 
-            diameter = max(diameter, left_path + right_path)
-            return max(left_path, right_path) + 1
-
-        longest_path(root)
+        longestPath(root)
         return diameter
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right = TreeNode(3)
