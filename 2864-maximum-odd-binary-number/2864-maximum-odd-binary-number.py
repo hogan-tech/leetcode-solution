4b8f7534f484b8fbf6646603e2ab664927692d06
Hogan <purmonth@gmail.com>
2024-03-01 18:10:41 +0800

    Time: 38 ms (59.91%), Space: 16.6 MB (45.47%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -1,9 +1,9 @@
-from collections import Counter
-
-
+# time complexity: O(n)
+# space complexity: O(n)
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        return '1' * (s.count('1') - 1) + '0' * (len(s) - s.count('1')) + '1'
+        oneFreq = s.count('1')
+        return '1' * (oneFreq - 1) + '0' * (len(s) - oneFreq) + '1'
 
 
 s = "010"
