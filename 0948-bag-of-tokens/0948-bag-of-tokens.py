b6a2015509bb8e7e8f832af76d22a434830ca1d5
Hogan <purmonth@gmail.com>
2024-03-04 23:10:50 +0800

    Time: 49 ms (81.30%), Space: 16.8 MB (64.63%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        i, j = 0, len(tokens) - 1
+        score = 0
+        while i <= j:
+            if tokens[i] <= power:
+                score += 1
+                power -= tokens[i]
+                i += 1
+            elif score >= 1 and i < j:
+                score -= 1
+                power += tokens[j]
+                j -= 1
+            else:
+                break
+        return score
+
+
+tokens = [100]
+power = 50
+print(Solution().bagOfTokensScore(tokens, power))
