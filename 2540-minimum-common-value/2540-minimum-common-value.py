9d30e7535aeabf2a1a79ea2d7915962b52c0681d
Hogan <purmonth@gmail.com>
2024-03-09 14:58:02 +0800

    Time: 346 ms (69.30%), Space: 42.7 MB (14.17%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        unionNums = set(nums1).intersection(set(nums2))
+        return min(unionNums) if unionNums else -1
+
+
+nums1 = [1, 2, 3, 6]
+nums2 = [2, 3, 4, 5]
+print(Solution().getCommon(nums1, nums2))
