6dd7f03b8d167c6c839756f42fba0108fecbeb9a
Hogan <purmonth@gmail.com>
2024-03-11 01:30:04 +0800

    Time: 46 ms (69.49%), Space: 16.7 MB (53.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,11 +1,13 @@
+# time complexity: O(m+n)
+# space complexity: O(m+n)
 from typing import List
 
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1).intersection(set(nums2)))
-    
 
-nums1 = [4,9,5]
-nums2 = [9,4,9,8,4]
-print(Solution().intersection(nums1,nums2))
+
+nums1 = [4, 9, 5]
+nums2 = [9, 4, 9, 8, 4]
+print(Solution().intersection(nums1, nums2))
