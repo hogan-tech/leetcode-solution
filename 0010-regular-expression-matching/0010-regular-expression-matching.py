fe0341fa77e337b8f1e00fe3d6b28b1e59c165fb
Hogan <purmonth@gmail.com>
2024-03-19 20:59:20 +0800

    Time: 9680 ms (5.01%), Space: 16.5 MB (95.99%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,14 +2,12 @@ class Solution(object):
     def isMatch(self, s: str, p: str) -> bool:
         if not p:
             return not s
-
-        first_match = bool(s) and p[0] in {s[0], '.'}
-
+        firstMatch = bool(s) and p[0] in {s[0], '.'}
         if len(p) >= 2 and p[1] == '*':
             return (self.isMatch(s, p[2:]) or
-                    first_match and self.isMatch(s[1:], p))
+                    firstMatch and self.isMatch(s[1:], p))
         else:
-            return first_match and self.isMatch(s[1:], p[1:])
+            return firstMatch and self.isMatch(s[1:], p[1:])
 
 
 s = "aa"
