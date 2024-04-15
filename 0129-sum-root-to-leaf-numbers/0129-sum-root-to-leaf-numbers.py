acffc517bc03effe27d4540d94b3000fb8721f2b
Hogan <purmonth@gmail.com>
2024-04-15 23:45:31 +0800

    Time: 34 ms (68.90%), Space: 16.5 MB (26.23%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,34 @@
+# time complexity: O(n)
+# space complexity: O(n)
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> List[int]:
+        sumNode = 0
+
+        def traverse(node: Optional[TreeNode], currNum: int):
+            nonlocal sumNode
+            if node is None:
+                return
+            currNum = currNum * 10 + node.val
+            if node.left is None and node.right is None:
+                sumNode += currNum
+            traverse(node.left, currNum)
+            traverse(node.right, currNum)
+        traverse(root, 0)
+        return sumNode
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+print(Solution().sumNumbers(root))
