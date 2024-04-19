0eb1af1174f434f9d28e7a8085efa6ad5f0a57b2
Hogan <purmonth@gmail.com>
2024-04-19 18:33:01 +0800

    200

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,38 @@
+# time complexity: O(m*n)
+# space complexity: O(m*n)
 from typing import List
 
 
 class Solution:
     def dfs(self, grid: List[List[str]], r: int, c: int) -> None:
-        nr = len(grid)
-        nc = len(grid[0])
+        nR = len(grid)
+        nC = len(grid[0])
         grid[r][c] = '0'
-        if r-1 >= 0 and grid[r-1][c] == '1':
-            self.dfs(grid, r-1, c)
-        if r+1 < nr and grid[r+1][c] == '1':
-            self.dfs(grid, r+1, c)
-        if c-1 >= 0 and grid[r][c-1] == '1':
-            self.dfs(grid, r, c-1)
-        if c+1 < nc and grid[r][c+1] == '1':
-            self.dfs(grid, r, c+1)
+        if r - 1 >= 0 and grid[r - 1][c] == "1":
+            self.dfs(grid, r - 1, c)
+        if r + 1 < nR and grid[r + 1][c] == "1":
+            self.dfs(grid, r + 1, c)
+        if c - 1 >= 0 and grid[r][c - 1] == "1":
+            self.dfs(grid, r, c - 1)
+        if c + 1 < nC and grid[r][c + 1] == "1":
+            self.dfs(grid, r, c + 1)
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        nr = len(grid)
-        if not nr:
+        if not grid:
             return 0
-        nc = len(grid[0])
-        num_islands = 0
-        for r in range(nr):
-            for c in range(nc):
+        numIslands = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c] == '1':
-                    num_islands += 1
+                    numIslands += 1
                     self.dfs(grid, r, c)
-        return num_islands
+        return numIslands
+
+
+grid = [
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]
+print(Solution().numIslands(grid))
