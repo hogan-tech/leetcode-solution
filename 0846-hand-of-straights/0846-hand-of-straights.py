f54a0a89049a1077830a2e1a51627e9ce50827ed
Hogan <purmonth@gmail.com>
2024-06-07 00:02:36 +0800

    Time: 152 ms (90.76%), Space: 18.3 MB (74.24%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,21 @@
+from typing import Counter, List
+
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hand.sort()
+        handCounter = Counter(hand)
+        if len(hand) % groupSize:
+            return False
+        for num in hand:
+            if handCounter[num]:
+                for currNum in range(num, num+groupSize):
+                    handCounter[currNum] -= 1
+                    if handCounter[currNum] < 0:
+                        return False
+        return True
+
+
+hand = [1, 2, 3, 6, 2, 3, 4, 7, 8]
+groupSize = 3
+print(Solution().isNStraightHand(hand, groupSize))
