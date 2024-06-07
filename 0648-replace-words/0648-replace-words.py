73d7f8be1b34ab6ef3e3a497beecab0ea8c3be45
Hogan <purmonth@gmail.com>
2024-06-07 13:48:40 +0800

    Time: 377 ms (11.09%), Space: 24.9 MB (94.66%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        dictSet = set(dictionary)
+        result = []
+        for word in sentence.split(" "):
+            for wordIdx in range(len(word)+1):
+                if word[:wordIdx] in dictSet:
+                    result.append(word[:wordIdx])
+                    break
+                if wordIdx == len(word):
+                    result.append(word[:wordIdx])
+            
+        return   " ".join(result)
+
+
+dictionary = ["cat","bat","rat"]
+sentence = "the cattle was rattled by the battery"
+print(Solution().replaceWords(dictionary, sentence))
