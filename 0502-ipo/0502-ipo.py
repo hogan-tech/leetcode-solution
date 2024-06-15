a6c679f0a9a30ca91acc72044423194a3c97ddd7
Hogan <purmonth@gmail.com>
2024-06-15 11:47:21 +0800

    Time: 746 ms (47.40%), Space: 41.3 MB (55.37%) - LeetHub

---
+++ 0502-ipo/0502-ipo.py
@@ -0,0 +1,26 @@
+# time complexity: O(nlogn)
+# space complexity: O(n)
+import heapq
+from typing import List
+
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        projects = []
+        for i in range(len(profits)):
+            heapq.heappush(projects, (capital[i], profits[i]))
+        available = []
+        for _ in range(k):
+            while projects and projects[0][0] <= w:
+                heapq.heappush(available, -heapq.heappop(projects)[1])
+            if len(available) == 0:
+                break
+            w -= heapq.heappop(available)
+        return w
+
+
+k = 2
+w = 0
+profits = [1, 2, 3]
+capital = [0, 1, 1]
+print(Solution().findMaximizedCapital(k, w, profits, capital))
