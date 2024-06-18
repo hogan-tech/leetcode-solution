6b1dbd4965263f06673a826be66ecaaf85c687c5
Hogan <purmonth@gmail.com>
2024-06-18 23:34:05 +0800

    Time: 540 ms (7.91%), Space: 19.6 MB (74.98%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,20 +1,37 @@
+# time complexity: O(nlogn + mlogn)
+# space complexity: O(n)
 from typing import List
 
 
 class Solution:
-    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        jobsNum = len(profit)
-        jobs = []
-        totalProfit = 0
-        for i in range(jobsNum):
-            jobs.append((difficulty[i], profit[i]))
-        jobs.sort(key=lambda job: job[1], reverse=True)
-        for i in range(len(worker)):
-            for j in range(jobsNum):
-                if worker[i] >= jobs[j][0]:
-                    totalProfit += jobs[j][1]
-                    break
-        return totalProfit
+    def maxProfitAssignment(
+        self, difficulty: List[int], profit: List[int], worker: List[int]
+    ) -> int:
+        jobProfile = [(0, 0)]
+        for i in range(len(difficulty)):
+            jobProfile.append((profit[i], difficulty[i]))
+
+        jobProfile.sort(reverse=True)
+        for i in range(len(jobProfile) - 1):
+            jobProfile[i + 1] = (
+                jobProfile[i + 1][0],
+                min(jobProfile[i][1], jobProfile[i + 1][1]),
+            )
+
+        netProfit = 0
+        for ability in worker:
+            left, right = 0, len(jobProfile) - 1
+            jobProfit = 0
+            while left <= right:
+                mid = (left + right) // 2
+                if jobProfile[mid][1] <= ability:
+                    jobProfit = max(jobProfit, jobProfile[mid][0])
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            netProfit += jobProfit
+
+        return netProfit
 
 
 difficulty = [85, 47, 57]
