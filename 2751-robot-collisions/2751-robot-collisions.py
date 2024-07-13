c6836141a07d7f2e5db542bc3d18da07b5461701
Hogan <purmonth@gmail.com>
2024-07-13 12:29:54 +0800

    Time: 957 ms (72.12%), Space: 34.3 MB (85.58%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+        stack = []
+        robotsIdx = list(range(len(positions)))
+        robotsIdx.sort(key=lambda x: positions[x])
+        for currIdx in robotsIdx:
+            if directions[currIdx] == "R":
+                stack.append(currIdx)
+            else:
+                while stack and healths[currIdx] > 0:
+                    topIdx = stack.pop()
+                    if healths[topIdx] > healths[currIdx]:
+                        healths[topIdx] -= 1
+                        healths[currIdx] = 0
+                        stack.append(topIdx)
+                    elif healths[topIdx] < healths[currIdx]:
+                        healths[currIdx] -= 1
+                        healths[topIdx] = 0
+                    else:
+                        healths[currIdx] = 0
+                        healths[topIdx] = 0
+
+        return [health for health in healths if health > 0]
+
+
+positions = [3, 5, 2, 6]
+healths = [10, 10, 15, 12]
+directions = "RLRL"
+print(Solution().survivedRobotsHealths(positions, healths, directions))
