4d8f9a3738411568ba8c9dd9636e62a62395d3a6
Hogan <purmonth@gmail.com>
2024-08-03 14:08:55 +0800

    Time: 66 ms (93.23%), Space: 16.8 MB (22.18%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -0,0 +1,11 @@
+from typing import Counter, List
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return Counter(target) == Counter(arr)
+
+
+target = [1, 2, 3, 4]
+arr = [2, 4, 1, 3]
+print(Solution().canBeEqual(target, arr))
