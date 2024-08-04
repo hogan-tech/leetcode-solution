7c618f11128121a056c81207852acfd172d259a2
Hogan <purmonth@gmail.com>
2024-08-04 15:26:08 +0800

    Time: 244 ms (42.03%), Space: 38 MB (67.70%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        subList = []
+        for i in range(len(nums)):
+            tempSum = 0
+            for j in range(i, len(nums)):
+                tempSum += nums[j]
+                subList.append(tempSum)
+        subList.sort()
+        ans = 0
+        for i in range(left-1, right):
+            mod = 10**9 + 7
+            ans = (ans + subList[i]) % mod
+        return ans
+
+
+nums = [1, 2, 3, 4]
+n = 4
+left = 1
+right = 10
+print(Solution().rangeSum(nums, n, left, right))
