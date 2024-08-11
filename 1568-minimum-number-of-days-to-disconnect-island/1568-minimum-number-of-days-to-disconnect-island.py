b14a0d3c6d473a0a029477f38721d6ebece78ce8
Hogan <purmonth@gmail.com>
2024-08-12 00:47:38 +0800

    Time: 1004 ms (32.71%), Space: 16.9 MB (49.53%) - LeetHub

--- 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.py
+++ 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.py
@@ -1,18 +1,23 @@
+# time complexity: O((mn)^2)
+# space complexity: O(m*n)
+from typing import List
+
+
 class Solution:
     def minDays(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
 
-        def _count_islands():
+        def countIslands():
             visited = set()
             count = 0
             for i in range(rows):
                 for j in range(cols):
                     if grid[i][j] == 1 and (i, j) not in visited:
-                        _explore_island(i, j, visited)
+                        exploreIsland(i, j, visited)
                         count += 1
             return count
 
-        def _explore_island(i, j, visited):
+        def exploreIsland(i, j, visited):
             if (
                 i < 0
                 or i >= rows
@@ -24,20 +29,21 @@ class Solution:
                 return
             visited.add((i, j))
             for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                _explore_island(i + di, j + dj, visited)
+                exploreIsland(i + di, j + dj, visited)
 
-        # Check if already disconnected
-        if _count_islands() != 1:
+        if countIslands() != 1:
             return 0
 
-        # Check if can be disconnected in 1 day
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 1:
                     grid[i][j] = 0
-                    if _count_islands() != 1:
+                    if countIslands() != 1:
                         return 1
                     grid[i][j] = 1
 
-        # If can't be disconnected in 0 or 1 day, return 2
         return 2
+
+
+grid = [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]
+print(Solution().minDays(grid))
