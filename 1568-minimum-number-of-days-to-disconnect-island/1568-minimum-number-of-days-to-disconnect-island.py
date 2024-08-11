888629ae29c0593d7a7fca2e245929b2d781eb25
Hogan <purmonth@gmail.com>
2024-08-12 00:46:28 +0800

    Time: 982 ms (38.32%), Space: 16.8 MB (49.53%) - LeetHub

---
+++ 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minDays(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        def _count_islands():
+            visited = set()
+            count = 0
+            for i in range(rows):
+                for j in range(cols):
+                    if grid[i][j] == 1 and (i, j) not in visited:
+                        _explore_island(i, j, visited)
+                        count += 1
+            return count
+
+        def _explore_island(i, j, visited):
+            if (
+                i < 0
+                or i >= rows
+                or j < 0
+                or j >= cols
+                or grid[i][j] == 0
+                or (i, j) in visited
+            ):
+                return
+            visited.add((i, j))
+            for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                _explore_island(i + di, j + dj, visited)
+
+        # Check if already disconnected
+        if _count_islands() != 1:
+            return 0
+
+        # Check if can be disconnected in 1 day
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    grid[i][j] = 0
+                    if _count_islands() != 1:
+                        return 1
+                    grid[i][j] = 1
+
+        # If can't be disconnected in 0 or 1 day, return 2
+        return 2
