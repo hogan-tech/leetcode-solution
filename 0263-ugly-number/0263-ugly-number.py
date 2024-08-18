74eca15cd8e930ad574f44774ba246bef7a01116
Hogan <purmonth@gmail.com>
2024-08-19 00:50:59 +0800

    Time: 24 ms (98.42%), Space: 16.6 MB (46.14%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 1:
+            return True
+        while n:
+            if n % 2 == 0:
+                n /= 2
+                continue
+            if n % 3 == 0:
+                n /= 3
+                continue
+            if n % 5 == 0:
+                n /= 5
+                continue
+            if n == 1:
+                return True
+            return False
+
+
+n = 14
+print(Solution().isUgly(n))
