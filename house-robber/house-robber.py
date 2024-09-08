1531678fa765b6e6e067bb3893df7e25a2bd1c12
Hogan <purmonth@gmail.com>
2024-09-08 16:09:07 -0400

    Time: 43 ms, Memory: 16.6 MB - LeetHub

---
+++ house-robber/house-robber.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[-1]
+
+
+nums = [2, 7, 9, 3, 1]
+print(Solution().rob(nums))
