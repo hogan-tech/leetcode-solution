4670d62089ff5aa7cb0c61de97e97186326d0fe0
Hogan <purmonth@gmail.com>
2024-09-09 14:56:39 -0400

    Time: 396 ms (95.45%), Space: 34 MB (10.53%) - LeetHub

---
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.py
@@ -0,0 +1,10 @@
+from typing import Counter, List
+
+
+class Solution:
+    def maximizeGreatness(self, nums: List[int]) -> int:
+        return len(nums) - max(Counter(nums).values())
+
+
+nums = [1, 3, 5, 2, 1, 3, 1]
+print(Solution().maximizeGreatness(nums))
