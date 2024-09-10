c48a4fa64c9e790270b12d99a1a7fbdce49d0d84
Hogan <purmonth@gmail.com>
2024-09-10 19:37:19 -0400

    Time: 26 ms (94.81%), Space: 16.6 MB (42.61%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.py
+++ 0400-nth-digit/0400-nth-digit.py
@@ -1,16 +1,19 @@
+# time complexity: O(n)
+# space complexity: O(1)
 class Solution:
     def findNthDigit(self, n: int) -> int:
-        if n <= 9:  # If n is a single digit, return n
+        if n < 10:
             return n
-        
-        base = 9  # Set the base value for the digit count
-        digits = 1  # Set the initial number of digits to 1
+        base = 9
+        digits = 1
         while n > base * digits:
-            n -= base * digits  # Subtract the count of digits from n
-            base *= 10  # Increase the base value by a factor of 10
-            digits += 1  # Increase the number of digits by 1
-            
-        num = 10 ** (digits - 1) + (n - 1) // digits  # Calculate the number where the nth digit is located
-        idx = (n - 1) % digits  # Calculate the index of the nth digit in the number
-        
-        return int(str(num)[idx])  # Return the nth digit as an integer
+            n -= base * digits
+            base *= 10
+            digits += 1
+
+        num = 10 ** (digits - 1) + (n - 1) // digits
+        idx = (n - 1) % digits
+        return int(str(num)[idx])
+    
+n = 3
+print(Solution().findNthDigit(n))
