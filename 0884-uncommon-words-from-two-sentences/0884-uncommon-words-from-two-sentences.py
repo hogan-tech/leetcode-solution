0da0d63f23ea8c14124f2dac255d8a738f63bf0f
Hogan <purmonth@gmail.com>
2024-09-16 23:25:52 -0400

    Time: 25 ms (97.81%), Space: 16.6 MB (5.76%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -0,0 +1,16 @@
+from typing import Counter, List
+
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        sList = list(s1.split(" ")) + list(s2.split(" "))
+        result = []
+        for item in Counter(sList).items():
+            if item[1] == 1:
+                result.append(item[0])
+        return result
+
+
+s1 = "this apple is sweet"
+s2 = "this apple is sour"
+print(Solution().uncommonFromSentences(s1, s2))
