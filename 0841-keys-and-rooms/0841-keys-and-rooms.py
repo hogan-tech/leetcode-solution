a84dc6e67062cc23b86eb2d73e0ae64d62339188
Hogan <purmonth@gmail.com>
2024-09-19 23:55:52 -0400

    Time: 59 ms (92.36%), Space: 17.1 MB (15.15%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        canVisit = [False] * len(rooms)
+        canVisit[0] = True
+        stack = [0]
+        while stack:
+            for nextIdx in rooms[stack.pop()]:
+                if canVisit[nextIdx] is False:
+                    canVisit[nextIdx] = True
+                    stack.append(nextIdx)
+        return all(canVisit)
+
+
+rooms = [[6, 7, 8], [5, 4, 9], [], [8], [4],
+         [], [1, 9, 2, 3], [7], [6, 5], [2, 3, 1]]
+print(Solution().canVisitAllRooms(rooms))
