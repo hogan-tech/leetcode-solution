4ca622ae661c21bef4eeffba2cbf54f70a27dd94
Hogan <purmonth@gmail.com>
2024-09-21 18:11:50 -0400

    Time: 87 ms (63.81%), Space: 16.6 MB (38.34%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        result = 0
+        left, right = 0,0
+        freq = {}
+        for right in range(len(s)):
+            if s[right] not in freq:
+                freq[s[right]] = 0
+            freq[s[right]] += 1
+
+            windowSize = right - left + 1
+
+            if windowSize - max(freq.values()) <= k:
+                result = max(result, windowSize)
+            else:
+                freq[s[left]] -= 1
+                if not freq[s[left]]:
+                    freq.pop(s[left])
+                left += 1
+
+        return result
+
+s = "AABABBA"
+k = 1
+print(Solution().characterReplacement(s, k))
