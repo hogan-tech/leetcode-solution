4dae362700547641b505659532a98171cdfdab26
Hogan <purmonth@gmail.com>
2024-09-25 10:55:02 -0400

    Time: 34 ms (87.72%), Space: 16.7 MB (5.54%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -0,0 +1,34 @@
+# time complexity: O(n)
+# space complexity: O(1)
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sentinel = ListNode(0, head)
+        pred = sentinel
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                pred.next = head.next
+            else:
+                pred = pred.next
+            head = head.next
+        return sentinel.next
+
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(3)
+head.next.next.next.next = ListNode(4)
+head.next.next.next.next.next = ListNode(4)
+head.next.next.next.next.next.next = ListNode(5)
+print(Solution().deleteDuplicates(head))
