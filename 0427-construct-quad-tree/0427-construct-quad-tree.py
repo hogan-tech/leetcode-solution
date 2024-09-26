e27293cdaf488b08eff13ff1a5406d8fbf6fa927
Hogan <purmonth@gmail.com>
2024-09-26 17:35:44 -0400

    Time: 89 ms (64.13%), Space: 17.4 MB (38.13%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Node:
+    def __init__(self, val, isLeaf, topLeft=None, topRight=None, bottomLeft=None, bottomRight=None):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        return self.helper(grid, 0, 0, len(grid))
+
+    def helper(self, grid, i, j, w):
+        if self.allSame(grid, i, j, w):
+            return Node(grid[i][j] == 1, True)
+
+        node = Node(True, False)
+        node.topLeft = self.helper(grid, i, j, w // 2)
+        node.topRight = self.helper(grid, i, j + w // 2, w // 2)
+        node.bottomLeft = self.helper(grid, i + w // 2, j, w // 2)
+        node.bottomRight = self.helper(grid, i + w // 2, j + w // 2, w // 2)
+        return node
+
+    def allSame(self, grid, i, j, w):
+        for x in range(i, i + w):
+            for y in range(j, j + w):
+                if grid[x][y] != grid[i][j]:
+                    return False
+        return True
+
+
+grid = [[0, 1], [1, 0]]
+print(Solution().construct(grid))
