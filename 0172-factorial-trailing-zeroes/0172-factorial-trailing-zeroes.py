4ba373422b44ef7b31fe2a222266ba07c7aa7be5
Hogan <purmonth@gmail.com>
2024-09-26 14:45:21 -0400

    Time: 9683 ms (5.06%), Space: 16.6 MB (29.18%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        total = 1
+        for i in range(2, n + 1):
+            total *= i
+        result = 0
+        while total % 10 == 0:
+            result += 1
+            total //= 10
+        return result
+
+
+n = 5
+print(Solution().trailingZeroes(n))
