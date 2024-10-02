d8ed8d48aedf50317a88d27f3289796db7c84a10
Hogan <purmonth@gmail.com>
2024-10-01 22:37:16 -0400

    Time: 38 ms (72.97%), Space: 16.5 MB (84.29%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
+
+
+num1 = "123"
+num2 = "456"
+print(Solution().multiply(num1, num2))
