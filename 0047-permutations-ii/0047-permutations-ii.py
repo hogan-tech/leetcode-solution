bb4425d1570faed3e03b6f6969f1aefbc28c3ec6
Hogan <purmonth@gmail.com>
2024-10-01 20:54:30 -0400

    Time: 574 ms (11.01%), Space: 22.2 MB (5.00%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        for i, item in enumerate(list(permutations(nums))):
+            temp = list(item)
+            if temp not in result:
+                result.append(temp)
+        return result
+
+
+nums = [1, 1, 2]
+print(Solution().permuteUnique(nums))
