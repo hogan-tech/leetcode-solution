2e0db054a55c202bed523c0ff0097b73e2bc3a34
Hogan <purmonth@gmail.com>
2024-10-05 11:07:10 -0400

    Time: 29 ms (86.47%), Space: 16.7 MB (20.47%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,40 @@
+# time complexity: O(1)
+# space complexity: O(1)
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        x, y = abs(a), abs(b)
+        # ensure that abs(a) >= abs(b)
+        if x < y:
+            return self.getSum(b, a)
+
+        # abs(a) >= abs(b) -->
+        # a determines the sign
+        sign = 1 if a > 0 else -1
+
+        if a * b >= 0:
+            # sum of two positive integers x + y
+            # where x > y
+            while y:
+                answer = x ^ y
+                carry = (x & y) << 1
+                x, y = answer, carry
+        else:
+            # difference of two integers x - y
+            # where x > y
+            while y:
+                answer = x ^ y
+                borrow = ((~x) & y) << 1
+                x, y = answer, borrow
+
+        return x * sign
+
+
+a = 1
+b = 2
+print(Solution().getSum(a, b))
+
+
+velocities = [0, 0.5, 1, 1.5]
+movement = velocities[1:3]
+
+print(movement)
