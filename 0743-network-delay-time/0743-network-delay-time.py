40de044d8410a663ca36f32c44aab4d55a29ea83
Hogan <purmonth@gmail.com>
2024-10-07 13:53:10 -0400

    Time: 379 ms (41.82%), Space: 18.4 MB (84.93%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,30 @@
+from collections import defaultdict, deque
+import heapq
+from typing import List
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        seen = set()
+        nodeMap = defaultdict(list)
+        for source, dest, time in times:
+            nodeMap[source].append((dest, time))
+
+        minHeap = [(0, k)]
+        print(minHeap)
+        heapq.heapify(minHeap)
+        while minHeap:
+            currentTime, node = heapq.heappop(minHeap)
+            seen.add(node)
+            if len(seen) == n:
+                return currentTime
+            for neighbor, time in nodeMap[node]:
+                if neighbor not in seen:
+                    heapq.heappush(minHeap, (currentTime + time, neighbor))
+        return -1
+
+
+times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
+n = 4
+k = 2
+print(Solution().networkDelayTime(times, n, k))
