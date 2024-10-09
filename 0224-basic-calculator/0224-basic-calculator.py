9c0761390e2cda878f7fdd41c8a276d6da12a7f7
Hogan <purmonth@gmail.com>
2024-10-09 10:30:44 -0400

    Time: 69 ms (51.14%), Space: 18 MB (42.17%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -21,24 +21,24 @@ class Solution:
         stack = []
         n, operand = 0, 0
         for i in range(len(s) - 1, -1, -1):
-            ch = s[i]
-            if ch.isdigit():
-                operand = (10 ** n * int(ch)) + operand
+            c = s[i]
+            if c.isdigit():
+                operand = (10**n * int(c)) + operand
                 n += 1
-            elif ch != " ":
+            elif c != " ":
                 if n:
                     stack.append(operand)
                     n, operand = 0, 0
-                if ch == '(':
+                if c == "(":
                     res = self.evaluateExpr(stack)
                     stack.pop()
                     stack.append(res)
                 else:
-                    stack.append(ch)
+                    stack.append(c)
         if n:
             stack.append(operand)
         return self.evaluateExpr(stack)
 
 
-s = "(1+(4+5+2)-3)+(6+8)"
+s = "1 + 1"
 print(Solution().calculate(s))
