505eff477e8743c0e8080c29275d2787e480d715
Hogan <purmonth@gmail.com>
2024-10-09 10:06:02 -0400

    Time: 65 ms (58.71%), Space: 18.1 MB (30.16%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,44 @@
+# time complexity: O(n)
+# space complexity: O(n)
+from typing import List
+
+
+class Solution:
+    def evaluateExpr(self, stack: List[int]):
+        if not stack or type(stack[-1]) == str:
+            stack.append(0)
+        res = stack.pop()
+
+        while stack and stack[-1] != ')':
+            sign = stack.pop()
+            if sign == '+':
+                res += stack.pop()
+            else:
+                res -= stack.pop()
+        return res
+
+    def calculate(self, s: str) -> int:
+        stack = []
+        n, operand = 0, 0
+        for i in range(len(s) - 1, -1, -1):
+            ch = s[i]
+            if ch.isdigit():
+                operand = (10 ** n * int(ch)) + operand
+                n += 1
+            elif ch != " ":
+                if n:
+                    stack.append(operand)
+                    n, operand = 0, 0
+                if ch == '(':
+                    res = self.evaluateExpr(stack)
+                    stack.pop()
+                    stack.append(res)
+                else:
+                    stack.append(ch)
+        if n:
+            stack.append(operand)
+        return self.evaluateExpr(stack)
+
+
+s = "(1+(4+5+2)-3)+(6+8)"
+print(Solution().calculate(s))
