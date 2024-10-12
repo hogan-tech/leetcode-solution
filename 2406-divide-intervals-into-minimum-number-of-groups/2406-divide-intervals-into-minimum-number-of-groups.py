204e642e0fbff7671105c018494297d655af84e1
Hogan <purmonth@gmail.com>
2024-10-12 14:59:36 -0400

    Time: 1124 ms (50.21%), Space: 56.3 MB (49.16%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -0,0 +1,17 @@
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        freeRooms = []
+        for i in range(len(intervals)):
+            if freeRooms and freeRooms[0] < intervals[i][0]:
+                heappop(freeRooms)
+            heappush(freeRooms, intervals[i][1])
+        return len(freeRooms)
+
+
+intervals = [[5, 10], [6, 8], [1, 5], [2, 3], [1, 10]]
+print(Solution().minGroups(intervals))
