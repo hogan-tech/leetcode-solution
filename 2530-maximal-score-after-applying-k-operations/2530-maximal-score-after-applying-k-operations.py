1eb2ee4eb85f726ffb08f8a3c0df1273b5071ae5
Hogan <purmonth@gmail.com>
2024-10-13 20:58:49 -0400

    Time: 734 ms (49.05%), Space: 31.1 MB (92.09%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
@@ -0,0 +1,21 @@
+from heapq import heapify, heappop, heappush
+from math import ceil
+from typing import List
+
+
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        pq = []
+        for num in nums:
+            heappush(pq, -num)
+        ans = 0
+        for _ in range(k):
+            maxNum = heappop(pq)
+            ans += -maxNum
+            heappush(pq, ceil(maxNum//3))
+        return ans
+
+
+nums = [1, 10, 3, 3, 3]
+k = 3
+print(Solution().maxKelements(nums, k))
