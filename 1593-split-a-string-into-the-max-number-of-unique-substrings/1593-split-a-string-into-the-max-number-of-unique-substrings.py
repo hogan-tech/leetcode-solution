2d41eb41092fd7f34a7d81be707ff70aa6969a0a
Hogan <purmonth@gmail.com>
2024-10-20 22:39:38 -0400

    Time: 158 ms (90.58%), Space: 16.5 MB (55.61%) - LeetHub

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def backtrack(self, s: str, start: int, seen: set) -> int:
+        if start == len(s):
+            return 0
+
+        maxCount = 0
+        for end in range(start + 1, len(s) + 1):
+            subString = s[start:end]
+            if subString not in seen:
+                seen.add(subString)
+                maxCount = max(maxCount, 1 + self.backtrack(s, end, seen))
+                seen.remove(subString)
+        
+        return maxCount
+
+    def maxUniqueSplit(self, s: str) -> int:
+        seen = set()
+        return self.backtrack(s, 0, seen)
+
+
+s = "ababccc"
+print(Solution().maxUniqueSplit(s))
