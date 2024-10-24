b77e8e4ec8c531bd041121d19f2b771282ed7d38
Hogan <purmonth@gmail.com>
2024-10-23 20:42:14 -0400

    Time: 0 ms (100.00%), Space: 16.7 MB (16.61%) - LeetHub

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def traverse(node):
+    if node is None:
+        return
+    if node.val:
+        print(node.val)
+    if node.left:
+        traverse(node.left)
+    if node.right:
+        traverse(node.right)
+
+
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 and not root2:
+            return True
+        if not root1 or not root2:
+            return False
+        if root1.val != root2.val:
+            return False
+
+        return (self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)) or \
+               (self.flipEquiv(root1.left, root2.right)
+                and self.flipEquiv(root1.right, root2.left))
+
+
+root1 = TreeNode(1)
+root1.left = TreeNode(2)
+root1.right = TreeNode(3)
+root1.left.left = TreeNode(4)
+root1.left.right = TreeNode(5)
+root1.right.left = TreeNode(6)
+root1.left.right.left = TreeNode(7)
+root1.left.right.right = TreeNode(8)
+
+
+root2 = TreeNode(1)
+root2.left = TreeNode(3)
+root2.right = TreeNode(2)
+root2.left.right = TreeNode(6)
+root2.right.left = TreeNode(4)
+root2.right.right = TreeNode(5)
+root2.right.right.left = TreeNode(8)
+root2.right.right.right = TreeNode(7)
+print(Solution().flipEquiv(root1, root2))
