1024cb0425dc55cd2e669914b6b6a3a4f472dc84
Hogan <purmonth@gmail.com>
2024-11-01 21:45:43 -0400

    Time: 0 ms (100.00%), Space: 16.4 MB (80.88%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        stack = []
+        for word in sentence.split(" "):
+            if stack and stack[-1] == word[0]:
+                stack.pop()
+            else:
+                stack.append(word[0])
+            stack.append(word[-1])
+        return stack[0] == stack[1] and len(stack) == 2
+
+
+sentence = "JuFZhkkASkRxIeiCOdGeELCMYHmuiqqLQqtjSxxHqnKrQOIEMxzkbapCCTvwaBlLXcGBVjqvppTJCttpbgbEcPEiSAcfcapXEUCdoQJvaUPCHRqVPuOghLqHWbFYgcMvxIufQTjpzmSMCAusXISLbJ aXOmiAAoYDyqqXwAe VnWA WZg uKlnD L UHRD"
+print(Solution().isCircularSentence(sentence))
+sentence = "leetcode exercises sound delightful"
+print(Solution().isCircularSentence(sentence))
