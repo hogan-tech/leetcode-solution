10764e838c10f192db58f1c4b22beeb46d8e5d55
Hogan <purmonth@gmail.com>
2024-11-05 15:43:53 -0500

    update readme

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
-                return True
-        return False
