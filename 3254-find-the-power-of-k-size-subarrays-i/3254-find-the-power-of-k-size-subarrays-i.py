703a652067b877e6a5fa269a9cc9ec1978b45efc
Hogan <purmonth@gmail.com>
2024-11-16 16:27:00 -0500

    Time: 72 ms (13.68%), Space: 16.9 MB (6.10%) - LeetHub

---
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        def resultPower(currIdx: int):
+            for i in range(1, k):
+                if nums[currIdx + i] != nums[currIdx + i-1] + 1:
+                    return -1
+            return max(nums[currIdx:currIdx + k])
+        result = []
+        for i in range(len(nums) - k + 1):
+            result.append(resultPower(i))
+        return result
+
+
+nums = [1, 2, 3, 4, 3, 2, 5]
+k = 3
+print(Solution().resultsArray(nums, k))
+nums = [2, 2, 2, 2, 2]
+k = 4
+print(Solution().resultsArray(nums, k))
+nums = [3, 2, 3, 2, 3, 2]
+k = 2
+print(Solution().resultsArray(nums, k))
