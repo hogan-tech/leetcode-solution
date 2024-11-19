54e17704dc31ba6b911d94fa52179ea5ea14e9c2
Hogan <purmonth@gmail.com>
2024-11-19 01:24:35 -0500

    Time: 135 ms (40.85%), Space: 36.4 MB (21.72%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        result = 0
+        freq = defaultdict(int)
+        count = 0
+        for i in range(k):
+            freq[nums[i]] += 1
+            count += nums[i]
+        if len(freq) == k:
+            result = count
+
+        for i in range(k, len(nums)):
+            freq[nums[i]] += 1
+            count += nums[i]
+            freq[nums[i-k]] -= 1
+            count -= nums[i-k]
+            if freq[nums[i-k]] == 0:
+                del freq[nums[i-k]]
+            if len(freq) == k:
+                result = max(result, count)
+        return result
+
+
+nums = [1, 5, 4, 2, 9, 9, 9]
+k = 3
+print(Solution().maximumSubarraySum(nums, k))
+nums = [4, 4, 4]
+k = 3
+print(Solution().maximumSubarraySum(nums, k))
+nums = [1, 1, 1, 7, 8, 9]
+k = 3
+print(Solution().maximumSubarraySum(nums, k))
