89df33cbc103f31730bea9d1c767ce14eaf8264d
Hogan <purmonth@gmail.com>
2024-11-26 11:12:21 -0500

    Time: 13 ms (41.72%), Space: 18.6 MB (9.65%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.py
+++ 2924-find-champion-ii/2924-find-champion-ii.py
@@ -1,7 +1,7 @@
+# time complexity: O(n)
+# space complexity: O(n)
 from collections import defaultdict
 from typing import List
-
-
 
 
 class Solution:
@@ -11,17 +11,16 @@ class Solution:
             indegree[i] = 0
         for edge in edges:
             indegree[edge[1]] += 1
-        
+
         champion = -1
         championCount = 0
         for key, count in indegree.items():
             if count == 0:
-                champion =  key
+                champion = key
                 championCount += 1
-            
+
         print(indegree)
         return champion if championCount == 1 else -1
-        
 
 
 n = 3
@@ -40,5 +39,5 @@ n = 2
 edges = []
 print(Solution().findChampion(n, edges))
 n = 3
-edges = [[0,1]]
+edges = [[0, 1]]
 print(Solution().findChampion(n, edges))
