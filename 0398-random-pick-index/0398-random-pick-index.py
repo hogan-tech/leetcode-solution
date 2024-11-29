32ec9a48d6058c69c7d00a13d39a7bf18eb6ce90
Hogan <purmonth@gmail.com>
2024-11-28 23:35:06 -0500

    Time: 37 ms (75.79%), Space: 28 MB (5.15%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+import random
+from typing import List
+
+
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.dict = defaultdict(list)
+        for i, num in enumerate(nums):
+            self.dict[num].append(i)
+
+    def pick(self, target: int) -> int:
+        targetList = self.dict[target]
+        randomNum = random.randint(0,len(targetList)-1)
+        return targetList[randomNum]
+
+
+# Your Solution object will be instantiated and called as such:
+obj = Solution([1, 2, 3, 3, 3])
+print(obj.pick(3))
+print(obj.pick(1))
+print(obj.pick(3))
+print(obj.pick(2))
+print(obj.pick(3))
+print(obj.pick(3))
+print(obj.pick(3))
