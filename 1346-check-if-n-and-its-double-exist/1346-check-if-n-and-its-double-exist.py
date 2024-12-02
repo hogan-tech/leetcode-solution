53d3315913b9d7e79f7fcf6a3b5ff32696084482
Hogan <purmonth@gmail.com>
2024-12-01 22:44:25 -0500

    Time: 12 ms (18.81%), Space: 17.4 MB (6.92%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if i == j:
+                    continue
+                if arr[i] == 2 * arr[j]:
+                    return True
+        return False
+
+
+arr = [10, 2, 5, 3]
+print(Solution().checkIfExist(arr))
+arr = [3, 1, 7, 11]
+print(Solution().checkIfExist(arr))
+arr = [7, 1, 14, 11]
+print(Solution().checkIfExist(arr))
