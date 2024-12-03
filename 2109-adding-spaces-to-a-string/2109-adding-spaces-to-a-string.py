7c981da939d65299db4ae0b4e812ceac2b83f838
Hogan <purmonth@gmail.com>
2024-12-02 22:10:54 -0500

    Time: 121 ms (36.15%), Space: 49 MB (40.48%) - LeetHub

---
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        result = ""
+        spaces = spaces[::-1]
+        for i, c in enumerate(s):
+            if spaces and i == spaces[-1]:
+                result += " "
+                spaces.pop()
+            result += c
+        return result
+
+
+s = "LeetcodeHelpsMeLearn"
+spaces = [8, 13, 15]
+print(Solution().addSpaces(s, spaces))
+s = "icodeinpython"
+spaces = [1, 5, 7, 9]
+print(Solution().addSpaces(s, spaces))
+s = "spacing"
+spaces = [0, 1, 2, 3, 4, 5, 6]
+print(Solution().addSpaces(s, spaces))
