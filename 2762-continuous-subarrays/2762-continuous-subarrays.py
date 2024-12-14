16b707db1bcd6190d7572c9e20355b40b6ef326e
Hogan <purmonth@gmail.com>
2024-12-14 08:53:34 -0500

    Time: 973 ms (16.27%), Space: 47.8 MB (5.73%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -0,0 +1,30 @@
+# time complexity: O(nlogn)
+# space complexity: O(n)
+from heapq import heapify, heappop, heappush
+from typing import List
+
+
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        left, ans = 0, 0
+        minHeap, maxHeap = [], []
+        heapify(minHeap)
+        heapify(maxHeap)
+
+        for right, num in enumerate(nums):
+            while minHeap and (minHeap[0][1] < left or abs(minHeap[0][0] - num) > 2):
+                _, idx = heappop(minHeap)
+                left = max(idx + 1, left)
+            while maxHeap and (maxHeap[0][1] < left or abs(-maxHeap[0][0] - num) > 2):
+                _, idx = heappop(maxHeap)
+                left = max(idx + 1, left)
+
+            heappush(minHeap, [num, right])
+            heappush(maxHeap, [-num, right])
+
+            ans += right - left + 1
+        return ans
+
+
+nums = [5, 4, 2, 4]
+print(Solution().continuousSubarrays(nums))
