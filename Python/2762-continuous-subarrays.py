3c9029cf017027b2e8515b6bc7de2e82821a6d1e
Hogan <purmonth@gmail.com>
2024-12-15 01:21:43 -0500

    update readme

--- Python/2762-continuous-subarrays.py
+++ Python/2762-continuous-subarrays.py
@@ -1,3 +1,5 @@
+# time complexity: O(nlogn)
+# space complexity: O(n)
 from heapq import heapify, heappop, heappush
 from typing import List
 
@@ -8,21 +10,21 @@ class Solution:
         minHeap, maxHeap = [], []
         heapify(minHeap)
         heapify(maxHeap)
-        
-        for right, num in enumerate( nums):
-            while minHeap and (minHeap[0][1] < left or abs( minHeap[0][0] - num) > 2):
+
+        for right, num in enumerate(nums):
+            while minHeap and (minHeap[0][1] < left or abs(minHeap[0][0] - num) > 2):
                 _, idx = heappop(minHeap)
                 left = max(idx + 1, left)
-            while maxHeap and (maxHeap[0][1] < left or abs (-maxHeap[0][0] - num) > 2):
+            while maxHeap and (maxHeap[0][1] < left or abs(-maxHeap[0][0] - num) > 2):
                 _, idx = heappop(maxHeap)
-                left = max(idx + 1,left)
+                left = max(idx + 1, left)
 
-            heappush(minHeap,[num,right])
-            heappush(maxHeap,[-num,right])
+            heappush(minHeap, [num, right])
+            heappush(maxHeap, [-num, right])
 
             ans += right - left + 1
         return ans
 
 
-nums = [5,4,2,4]
+nums = [5, 4, 2, 4]
 print(Solution().continuousSubarrays(nums))
