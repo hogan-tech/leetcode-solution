e5af21304b134f66da2d61bde6ef5b93c3b95a35
Hogan <purmonth@gmail.com>
2024-12-20 18:11:43 -0500

    Time: 19 ms (96.81%), Space: 29.3 MB (6.89%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        result = set()
+        visited = set()
+        currString = s[:10]
+        visited.add(currString)
+        for i in range(1,len(s) - 9):
+            currString = currString[1:] + s[i+9]
+            if currString in visited:
+                result.add(currString)
+            else:
+                visited.add(currString)
+        return list(result)
+
+
+s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+print(Solution().findRepeatedDnaSequences(s))
+s = "AAAAAAAAAAAAA"
+print(Solution().findRepeatedDnaSequences(s))
+s = "AAAAAAAAAAA"
+print(Solution().findRepeatedDnaSequences(s))
