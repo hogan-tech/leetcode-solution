14fe683ec4164fbee0011559ff9605f66df71e44
Hogan <purmonth@gmail.com>
2024-12-20 19:46:14 -0500

    Time: 55 ms (84.75%), Space: 17.7 MB (42.52%) - LeetHub

--- 0727-minimum-window-subsequence/0727-minimum-window-subsequence.py
+++ 0727-minimum-window-subsequence/0727-minimum-window-subsequence.py
@@ -2,27 +2,31 @@
 # space complexity: O(m*n)
 class Solution:
     def minWindow(self, s1: str, s2: str) -> str:
-        minSubLength = float('inf')
         minSubsequence = ""
-        sizeS1, sizeS2 = len(s1), len(s2)
-        idxS1, idxS2 = 0, 0
+        minSubLength = float('inf')
+        sizeS1 = len(s1)
+        sizeS2 = len(s2)
+        idxS1 = 0
+        idxS2 = 0
         while idxS1 < sizeS1:
             if s1[idxS1] == s2[idxS2]:
                 idxS2 += 1
                 if idxS2 == sizeS2:
-                    start, end = idxS1, idxS1
+                    start = idxS1
+                    end = idxS1
                     idxS2 -= 1
                     while idxS2 >= 0:
-                        if s1[start] == s2[idxS2]:
+                        if s2[idxS2] == s1[start]:
                             idxS2 -= 1
                         start -= 1
                     start += 1
-                    if end - start < minSubLength:
-                        minSubLength = end-start
-                        minSubsequence = s1[start: end + 1]
+                    currLength = end - start
+                    if currLength < minSubLength:
+                        minSubLength = currLength
+                        minSubsequence = s1[start:end+1]
                     idxS1 = start
-                    idxS2 = 0
             idxS1 += 1
+
         return minSubsequence
 
 
