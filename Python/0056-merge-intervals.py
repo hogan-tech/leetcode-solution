1bccf8ff408b101fb333030a92043b22b6b00970
Hogan <purmonth@gmail.com>
2024-12-22 22:17:00 -0500

    update readme

--- Python/0056-merge-intervals.py
+++ Python/0056-merge-intervals.py
@@ -1,13 +1,25 @@
+# time complexity: O(nlogn)
+# space complexity: O(n)
 from typing import List
 
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        merged = [intervals[0]]
-        for item in intervals:
-            if merged[-1][1] < item[0]:
-                merged.append(item)
+        mergeResult = [intervals[0]]
+        for interval in intervals:
+            if mergeResult[-1][1] < interval[0]:
+                mergeResult.append(interval)
             else:
-                merged[-1][1] = max(merged[-1][1], item[1])
-        return merged
+                mergeResult[-1][1] = max(mergeResult[-1][1], interval[1])
+        return mergeResult
+
+
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(Solution().merge(intervals))
+intervals = [[1, 4], [4, 5]]
+print(Solution().merge(intervals))
+intervals = [[1, 4], [2, 3]]
+print(Solution().merge(intervals))
+intervals = [[1, 4], [1, 4]]
+print(Solution().merge(intervals))
