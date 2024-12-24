a422e9df332a5dd1afe570a38c2032606fbc4f07
Hogan <purmonth@gmail.com>
2024-12-24 13:53:29 -0500

    update readme

--- Python/0206-reverse-linked-list.py
+++ Python/0206-reverse-linked-list.py
@@ -1,15 +1,42 @@
+# time complexity: O(n)
+# space complexity: O(1)
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prevNode = None
-        currNode = head
-        while currNode != None:
-            nextNode = currNode.next
-            currNode.next = prevNode
-            prevNode = currNode
-            currNode = nextNode
-        return prevNode
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
+
+
+
+# class Solution:
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         if (not head) or (not head.next):
+#             return head
+
+#         p = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return p
+
+root = ListNode(1)
+root.next = ListNode(2)
+root.next.next = ListNode(3)
+root.next.next.next = ListNode(4)
+root.next.next.next.next = ListNode(5)
+
+print(Solution().reverseList(root))
