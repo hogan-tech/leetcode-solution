5b907ecdffb6e4052154994b70c754d017c8a55a
Hogan <purmonth@gmail.com>
2024-12-24 15:46:19 -0500

    update readme

--- Python/0143-reorder-list.py
+++ Python/0143-reorder-list.py
@@ -13,19 +13,23 @@ class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         if not head:
             return
-        slow = fast = head
+        fast = head
+        slow = head
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
+            slow = slow.next
 
-        prev, curr = None, slow
+        prev = None
+        curr = slow
         while curr:
             curr.next, prev, curr = prev, curr, curr.next
 
-        first, second = head, prev
+        first = head
+        second = prev
         while second.next:
             first.next, first = second, first.next
             second.next, second = first, second.next
+
         return
 
 
