8bc1163202420b52a04bf833a2a414f08c5cfa9f
Hogan <purmonth@gmail.com>
2024-12-26 16:31:45 -0500

    update readme

--- Python/0725-split-linked-list-in-parts.py
+++ Python/0725-split-linked-list-in-parts.py
@@ -1,4 +1,5 @@
-# Definition for singly-linked list.
+# time complexity: O(n)
+# space complexity: O(1)
 from typing import List, Optional
 
 
@@ -10,21 +11,34 @@ class ListNode:
 
 class Solution:
     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
-        cur = head
-        for N in range(1001):
-            if not cur:
-                break
-            cur = cur.next
-        width, remainder = divmod(N, k)
+        result = [None] * k
+        curr = head
+        count = 0
+        while curr:
+            curr = curr.next
+            count += 1
+        width, remainder = divmod(count, k)
 
-        ans = []
-        cur = head
+        curr = head
+        prev = curr
         for i in range(k):
-            root = cur
-            for j in range(width + (i < remainder) - 1):
-                if cur:
-                    cur = cur.next
-            if cur:
-                cur.next, cur = None, cur.next
-            ans.append(root)
-        return ans
+            new = curr
+            currSize = width
+            if remainder > 0:
+                remainder -= 1
+                currSize += 1
+            for _ in range(currSize):
+                prev = curr
+                if curr:
+                    curr = curr.next
+            if prev is not None:
+                prev.next = None
+            result[i] = new
+        return result
+
+
+node = ListNode(1)
+node.next = ListNode(2)
+node.next.next = ListNode(3)
+k = 5
+print(Solution().splitListToParts(node, k))
