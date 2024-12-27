b7e881d140d41c288c2664d09534cbeb7a2ef288
Hogan <purmonth@gmail.com>
2024-12-27 17:25:09 -0500

    update readme

--- Python/0295-find-median-from-data-stream.py
+++ Python/0295-find-median-from-data-stream.py
@@ -1,19 +1,37 @@
+# time complexity: O(logn)
+# space complexity: O(n)
 import heapq
 
 
 class MedianFinder:
 
     def __init__(self):
-        self.heaps = [], []
+        self.largeMinHeap = []
+        self.smallMaxHeap = []
 
     def addNum(self, num: int) -> None:
-        small, large = self.heaps
-        heapq.heappush(small, -heapq.heappushpop(large, num))
-        if len(large) < len(small):
-            heapq.heappush(large, -heapq.heappop(small))
+        if not self.smallMaxHeap or -self.smallMaxHeap[0] >= num:
+            heapq.heappush(self.smallMaxHeap, -num)
+        else:
+            heapq.heappush(self.largeMinHeap, num)
+
+        if len(self.smallMaxHeap) > len(self.largeMinHeap) + 1:
+            heapq.heappush(self.largeMinHeap, -
+                           heapq.heappop(self.smallMaxHeap))
+        elif len(self.smallMaxHeap) < len(self.largeMinHeap):
+            heapq.heappush(self.smallMaxHeap, -
+                           heapq.heappop(self.largeMinHeap))
+        return
 
     def findMedian(self) -> float:
-        small, large = self.heaps
-        if len(large) > len(small):
-            return float(large[0])
-        return (large[0] - small[0]) / 2.0
+        if len(self.largeMinHeap) == len(self.smallMaxHeap):
+            return (self.largeMinHeap[0] - self.smallMaxHeap[0]) / 2.0
+        return float(-self.smallMaxHeap[0])
+
+
+medianFinder = MedianFinder()
+medianFinder.addNum(1)
+medianFinder.addNum(2)
+print(medianFinder.findMedian())
+medianFinder.addNum(3)
+print(medianFinder.findMedian())
