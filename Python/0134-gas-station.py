a2035a62de82558f9f96efd598840d91bc9cb799
Hogan <purmonth@gmail.com>
2024-12-31 13:53:28 -0500

    update readme

--- Python/0134-gas-station.py
+++ Python/0134-gas-station.py
@@ -5,22 +5,22 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        totalGain = 0
-        currGain = 0
-        answer = 0
-
+        if sum(cost) > sum(gas):
+            return -1
+        startIdx = 0
+        currentGas = 0
         for i in range(len(gas)):
-            totalGain += gas[i] - cost[i]
-            currGain += gas[i] - cost[i]
+            currentGas = currentGas + (gas[i] - cost[i])
+            if currentGas < 0:
+                currentGas = 0
+                startIdx = i + 1
 
-            if currGain < 0:
-                currGain = 0
-                answer = i + 1
-
-        return answer if totalGain >= 0 else -1
+        return startIdx
 
 
 gas = [1, 2, 3, 4, 5]
 cost = [3, 4, 5, 1, 2]
-
+print(Solution().canCompleteCircuit(gas, cost))
+gas = [2, 3, 4]
+cost = [3, 4, 3]
 print(Solution().canCompleteCircuit(gas, cost))
--- Readme/0134-gas-station.md
+++ Readme/0134-gas-station.md
@@ -1,4 +1,5 @@
-<h2><a href="https://leetcode.com/problems/gas-station/">134. Gas Station</a></h2><h3>Medium</h3><hr><div><p>There are <code>n</code> gas stations along a circular route, where the amount of gas at the <code>i<sup>th</sup></code> station is <code>gas[i]</code>.</p>
+<h2> 12387 1244
+134. Gas Station</h2><hr><div><p>There are <code>n</code> gas stations along a circular route, where the amount of gas at the <code>i<sup>th</sup></code> station is <code>gas[i]</code>.</p>
 
 <p>You have a car with an unlimited gas tank and it costs <code>cost[i]</code> of gas to travel from the <code>i<sup>th</sup></code> station to its next <code>(i + 1)<sup>th</sup></code> station. You begin the journey with an empty tank at one of the gas stations.</p>
 
