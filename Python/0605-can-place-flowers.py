ff4ce2faf7a1847b1094ea4c44d91438ff30f887
Hogan <purmonth@gmail.com>
2025-01-01 13:55:16 -0500

    update readme

--- Python/0605-can-place-flowers.py
+++ Python/0605-can-place-flowers.py
@@ -5,17 +5,23 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count = 0
+        leftPot = False
+        rightPot = False
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0:
-                leftpot = (i == 0) or (flowerbed[i-1] == 0)
-                rightpot = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0)
-                if leftpot and rightpot:
+                leftPot = i == 0 or flowerbed[i - 1] == 0
+                rightPot = i == len(flowerbed) - 1 or flowerbed[i + 1] == 0
+                if rightPot and leftPot:
                     flowerbed[i] += 1
                     n -= 1
-        return count >= n
+            if n <= 0:
+                return True
+        return False
 
 
 flowerbed = [1, 0, 0, 0, 0]
+n = 2
+print(Solution().canPlaceFlowers(flowerbed, n))
+flowerbed = [1, 0, 0, 0, 1]
 n = 2
 print(Solution().canPlaceFlowers(flowerbed, n))
--- Readme/0605-can-place-flowers.md
+++ Readme/0605-can-place-flowers.md
@@ -1,4 +1,5 @@
-<h2><a href="https://leetcode.com/problems/can-place-flowers/">605. Can Place Flowers</a></h2><h3>Easy</h3><hr><div><p>You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in <strong>adjacent</strong> plots.</p>
+<h2> 6800 1227
+605. Can Place Flowers</h2><hr><div><p>You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in <strong>adjacent</strong> plots.</p>
 
 <p>Given an integer array <code>flowerbed</code> containing <code>0</code>'s and <code>1</code>'s, where <code>0</code> means empty and <code>1</code> means not empty, and an integer <code>n</code>, return <code>true</code>&nbsp;<em>if</em> <code>n</code> <em>new flowers can be planted in the</em> <code>flowerbed</code> <em>without violating the no-adjacent-flowers rule and</em> <code>false</code> <em>otherwise</em>.</p>
 
