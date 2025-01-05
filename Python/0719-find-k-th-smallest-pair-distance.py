047f7eb1aac96639697cd98bb5a2370ec6af2d22
Hogan <purmonth@gmail.com>
2025-01-05 13:24:13 -0500

    update readme

--- Python/0719-find-k-th-smallest-pair-distance.py
+++ Python/0719-find-k-th-smallest-pair-distance.py
@@ -5,27 +5,34 @@ from typing import List
 
 class Solution:
     def smallestDistancePair(self, numbers: List[int], k: int) -> int:
-        numbers.sort()
-        minDistance, maxDistance = 0, numbers[-1] - numbers[0]
+        def countPairWithDistance(numbers: List[int], targetDistance: int):
+            left = 0
+            count = 0
+            for right in range(1, len(numbers)):
+                while numbers[right] - numbers[left] > targetDistance:
+                    left += 1
+                count += right - left
+            return count
 
+        numbers.sort()
+        minDistance = 0
+        maxDistance = numbers[-1] - numbers[0]
         while minDistance < maxDistance:
-            midDistance = (minDistance + maxDistance) // 2
-            if self.countPairsWithinDistance(numbers, midDistance) < k:
+            midDistance = minDistance + (maxDistance - minDistance) // 2
+            if countPairWithDistance(numbers, midDistance) < k:
                 minDistance = midDistance + 1
             else:
                 maxDistance = midDistance
 
         return minDistance
 
-    def countPairsWithinDistance(self, numbers: List[int], targetDistance: int) -> int:
-        count = left = 0
-        for right in range(1, len(numbers)):
-            while numbers[right] - numbers[left] > targetDistance:
-                left += 1
-            count += right - left
-        return count
-
 
 nums = [1, 3, 1]
 k = 1
-print(Solution().countPairsWithinDistance(nums, k))
+print(Solution().smallestDistancePair(nums, k))
+nums = [1, 1, 1]
+k = 2
+print(Solution().smallestDistancePair(nums, k))
+nums = [1, 6, 1]
+k = 3
+print(Solution().smallestDistancePair(nums, k))
