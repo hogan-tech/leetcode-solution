a14f244f653b5b68d68d84a40515f41a9112cac4
Hogan <purmonth@gmail.com>
2025-01-05 23:30:57 -0500

    update readme

--- Python/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ Python/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -6,18 +6,20 @@ from typing import List
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
         count = 0
-        for row in range(len(grid)):
+        for row in grid:
             left = 0
-            right = len(grid[0]) - 1
+            right = len(row) - 1
             while left <= right:
                 mid = left + (right - left) // 2
-                if grid[row][mid] < 0:
+                if row[mid] < 0:
                     right = mid - 1
                 else:
                     left = mid + 1
-            count += (len(grid[0]) - left)
+            count += (len(row) - left)
         return count
 
 
+grid = [[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]]
+print(Solution().countNegatives(grid))
 grid = [[3, 2], [1, 0]]
 print(Solution().countNegatives(grid))
--- Readme/1351-count-negative-numbers-in-a-sorted-matrix.md
+++ Readme/1351-count-negative-numbers-in-a-sorted-matrix.md
@@ -1,4 +1,5 @@
-<h2><a href="https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/">1351. Count Negative Numbers in a Sorted Matrix</a></h2><h3>Easy</h3><hr><div><p>Given a <code>m x n</code> matrix <code>grid</code> which is sorted in non-increasing order both row-wise and column-wise, return <em>the number of <strong>negative</strong> numbers in</em> <code>grid</code>.</p>
+<h2> 5022 133
+1351. Count Negative Numbers in a Sorted Matrix</h2><hr><div><p>Given a <code>m x n</code> matrix <code>grid</code> which is sorted in non-increasing order both row-wise and column-wise, return <em>the number of <strong>negative</strong> numbers in</em> <code>grid</code>.</p>
 
 <p>&nbsp;</p>
 <p><strong class="example">Example 1:</strong></p>
