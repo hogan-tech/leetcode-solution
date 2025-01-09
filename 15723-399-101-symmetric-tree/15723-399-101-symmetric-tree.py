d9f407c58abfa0371f4e66d4d500bf6fbdefe6fe
Hogan <purmonth@gmail.com>
2025-01-09 16:43:10 -0500

    Time: 0 ms, Memory: 17.9 MB - LeetHub

---
+++ 15723-399-101-symmetric-tree/15723-399-101-symmetric-tree.py
@@ -0,0 +1,31 @@
+# time complexity: O(n)
+# space complexity: O(n)
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node1: Optional[TreeNode], node2: Optional[TreeNode]):
+            if node1 is None and node2 is None:
+                return True
+            if node1 is None or node2 is None:
+                return False
+            return node1.val == node2.val and dfs(node1.left, node2.right) and dfs(node1.right, node2.left)
+        return dfs(root, root)
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(2)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(4)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(3)
+print(Solution().isSymmetric(root))
