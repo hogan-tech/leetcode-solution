5efbf511429ab64c68d5143379d004bc86e67ea0
Hogan <purmonth@gmail.com>
2025-01-10 20:09:44 -0500

    update readme

--- Python/0791-custom-sort-string.py
+++ Python/0791-custom-sort-string.py
@@ -1,13 +1,34 @@
 # time complexity: O(nlogn)
 # space complexity: O(n)
+from collections import defaultdict
+
+
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        charIdx = {char: index for index, char in enumerate(order)}
+        charIdx = {char: idx for idx, char in enumerate(order)}
 
         def customSort(char):
             return charIdx.get(char, float('inf'))
         sortedString = sorted(s, key=customSort)
         return "".join(sortedString)
+
+# time complexity: O(n)
+# space complexity: O(1)
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        freq = defaultdict(int)
+        for c in s:
+            freq[c] += 1
+        result = []
+        for c in order:
+            if c in freq:
+                result.append(c * freq[c])
+                del freq[c]
+
+        for c, count in freq.items():
+            result.append((c * count))
+
+        return ''.join(result)
 
 
 order = "cba"
