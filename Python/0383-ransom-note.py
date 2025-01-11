3cd7229cb75933f71a099deccd0fe14d182c9fa9
Hogan <purmonth@gmail.com>
2025-01-11 14:28:45 -0500

    update readme

--- Python/0383-ransom-note.py
+++ Python/0383-ransom-note.py
@@ -1,8 +1,25 @@
+# time complexity: O(n)
+# space complexity: O(1)
+from collections import Counter
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for c in ransomNote:
-            if (c not in magazine):
-                return False
-            location = magazine.index(c)
-            magazine = magazine[:location] + magazine[location+1:]
-        return True
+        ransomNoteCounter = Counter(ransomNote)
+        magazineCounter = Counter(magazine)
+        for key, freq in magazineCounter.items():
+            if key in ransomNoteCounter:
+                ransomNoteCounter[key] -= freq
+                if ransomNoteCounter[key] <= 0:
+                    del ransomNoteCounter[key]
+        return len(ransomNoteCounter) == 0
+
+
+ransomNote = "a"
+magazine = "b"
+print(Solution().canConstruct(ransomNote, magazine))
+ransomNote = "aa"
+magazine = "ab"
+print(Solution().canConstruct(ransomNote, magazine))
+ransomNote = "aa"
+magazine = "aab"
+print(Solution().canConstruct(ransomNote, magazine))
