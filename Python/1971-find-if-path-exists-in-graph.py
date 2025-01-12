b2962d689725d263b34228d6fb9e087ad7fe2ec4
Hogan <purmonth@gmail.com>
2025-01-12 00:12:59 -0500

    update readme

--- Python/1971-find-if-path-exists-in-graph.py
+++ Python/1971-find-if-path-exists-in-graph.py
@@ -24,9 +24,43 @@ class Solution:
             return False
         return dfs(source)
 
+# time complexity: O(m*a(n))
+# space complexity: O(n)
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [num for num in range(n + 1)]
+        self.rank = [1] * n
+
+    def find(self, num):
+        if num != self.parent[num]:
+            self.parent[num] = self.find(self.parent[num])
+        return self.parent[num]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] < self.rank[rootY]:
+                rootX, rootY = rootY, rootX
+            self.parent[rootY] = rootX
+            self.rank[rootX] += self.rank[rootY]
+
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        uf = UnionFind(n)
+        for x, y in edges:
+            uf.union(x, y)
+        return uf.find(source) == uf.find(destination)
+
 
 n = 3
 edges = [[0, 1], [1, 2], [2, 0]]
 source = 0
 destination = 2
+print(Solution().validPath(n, edges, source, destination))
+n = 6
+edges = [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]]
+source = 0
+destination = 5
 print(Solution().validPath(n, edges, source, destination))
