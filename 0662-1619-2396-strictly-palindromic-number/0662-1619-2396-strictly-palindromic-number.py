f63b043df984c5d9ed9dd0b830a4073a17f81fbe
Hogan <purmonth@gmail.com>
2025-02-05 13:41:43 -0500

    Time: 0 ms, Memory: 17.5 MB - LeetHub

---
+++ 0662-1619-2396-strictly-palindromic-number/0662-1619-2396-strictly-palindromic-number.py
@@ -0,0 +1,12 @@
+# time complexity: O(1)
+# space complexity: O(1)
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+
+
+n = 9
+print(Solution().isStrictlyPalindromic(n))
+n = 4
+print(Solution().isStrictlyPalindromic(n))
