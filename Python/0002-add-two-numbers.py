29a153705debe9f9c82cc900fe0020d04c30210f
Hogan <purmonth@gmail.com>
2025-02-09 14:29:00 -0500

    update readme

--- Python/0002-add-two-numbers.py
+++ Python/0002-add-two-numbers.py
@@ -3,24 +3,26 @@
 from typing import Optional
 
 
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        result = ListNode(0)
-        resultTail = result
+        curr = dummy = ListNode()
+
         carry = 0
-
         while l1 or l2 or carry:
-            val1 = (l1.val if l1 else 0)
-            val2 = (l2.val if l2 else 0)
-            carry, out = divmod(val1+val2 + carry, 10)
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            currNum = val1 + val2 + carry
+            carry, currNum = divmod(currNum, 10)
 
-            resultTail.next = ListNode(out)
-            resultTail = resultTail.next
+            curr.next = ListNode(currNum)
+            curr = curr.next
 
-            l1 = (l1.next if l1 else None)
-            l2 = (l2.next if l2 else None)
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
-        return result.next
+        
+        return dummy.next
 
 
 l1 = ListNode(0)
--- Readme/0002-add-two-numbers.md
+++ Readme/0002-add-two-numbers.md
@@ -1,4 +1,5 @@
-<h2><a href="https://leetcode.com/problems/add-two-numbers/">2. Add Two Numbers</a></h2><h3>Medium</h3><hr><div><p>You are given two <strong>non-empty</strong> linked lists representing two non-negative integers. The digits are stored in <strong>reverse order</strong>, and each of their nodes contains a single digit. Add the two numbers and return the sum&nbsp;as a linked list.</p>
+<h2> 32767 6576
+2. Add Two Numbers</h2><hr><div><p>You are given two <strong>non-empty</strong> linked lists representing two non-negative integers. The digits are stored in <strong>reverse order</strong>, and each of their nodes contains a single digit. Add the two numbers and return the sum&nbsp;as a linked list.</p>
 
 <p>You may assume the two numbers do not contain any leading zero, except the number 0 itself.</p>
 
