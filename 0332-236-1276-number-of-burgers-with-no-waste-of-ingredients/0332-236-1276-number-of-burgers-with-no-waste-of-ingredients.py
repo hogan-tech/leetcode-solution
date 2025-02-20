7bb6dbca04bdc076c9dc2e07a3223240930fab26
Hogan <purmonth@gmail.com>
2025-02-20 16:03:02 -0500

    Time: 3 ms, Memory: 17.7 MB - LeetHub

---
+++ 0332-236-1276-number-of-burgers-with-no-waste-of-ingredients/0332-236-1276-number-of-burgers-with-no-waste-of-ingredients.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def numOfBurgers(self, tomatoSlices: int, cheeseSlices: int) -> List[int]:
+        if tomatoSlices == 0 and cheeseSlices == 0:
+            return [0, 0]
+        x = (tomatoSlices - 2 * cheeseSlices) / 2
+        y = cheeseSlices - x
+        return [int(x), int(y)] if (x >= 0 and y >= 0) and x == int(x) and y == int(y) else []
+
+
+# Jumbo = X
+# Small = Y
+# 4X + 2Y = tomatoSlices
+# X + Y = cheeseSlices
+# X = (tomato - 2cheeseSlices) / 2
+# y = cheeseSlices - X
+# return [X, Y]
+
+tomatoSlices = 16
+cheeseSlices = 7
+print(Solution().numOfBurgers(tomatoSlices, cheeseSlices))
+tomatoSlices = 17
+cheeseSlices = 4
+print(Solution().numOfBurgers(tomatoSlices, cheeseSlices))
+tomatoSlices = 4
+cheeseSlices = 17
+print(Solution().numOfBurgers(tomatoSlices, cheeseSlices))
+tomatoSlices = 2
+cheeseSlices = 1
+print(Solution().numOfBurgers(tomatoSlices, cheeseSlices))
