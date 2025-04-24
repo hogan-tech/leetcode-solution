82cec4682fe20e2a3a2f2f5af49a7c1dbcff364e
Hogan <purmonth@gmail.com>
2025-04-23 20:12:51 -0400

    Time: 152 ms (30.53%), Space: 33.9 MB (10%) - LeetHub

---
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        midIdx = len(nums) // 2
+        smallNums = nums[:midIdx]
+        largeNums = nums[midIdx:]
+        result = [0 for _ in range(len(nums))]
+        for i in range(len(nums)):
+            result[i] = smallNums.pop() if i % 2 else largeNums.pop()
+        return result
+
+
+nums = [1, 2, 3, 4, 5]
+print(Solution().rearrangeArray(nums))
+nums = [6, 2, 0, 9, 7]
+print(Solution().rearrangeArray(nums))
