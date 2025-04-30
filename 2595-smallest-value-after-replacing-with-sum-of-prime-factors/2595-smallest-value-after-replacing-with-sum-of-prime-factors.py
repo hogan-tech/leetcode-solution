ebd9234a8b07def07a3055aaf43b3e004113e1e3
Hogan <purmonth@gmail.com>
2025-04-30 19:04:40 -0400

    Time: 0 ms (100%), Space: 17.8 MB (48.39%) - LeetHub

---
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.py
@@ -0,0 +1,34 @@
+# time complexity: O(nlogn)
+# space complexity: O(n)
+class Solution:
+    def smallestValue(self, n: int) -> int:
+        def primeFactors(n):
+            i = 2
+            factors = []
+            while i * i <= n:
+                if n % i:
+                    i += 1
+                else:
+                    n //= i
+                    factors.append(i)
+            if n > 1:
+                factors.append(n)
+            return factors
+
+        num = n
+        seen = set()
+        while num > 0:
+            factors = primeFactors(num)
+            if len(factors) == 1:
+                return factors[0]
+            num = sum(factors)
+            if num in seen:
+                break
+            seen.add(num)
+        return num
+
+
+n = 15
+print(Solution().smallestValue(n))
+n = 3
+print(Solution().smallestValue(n))
