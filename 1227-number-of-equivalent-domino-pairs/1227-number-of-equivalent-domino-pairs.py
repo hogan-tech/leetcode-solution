5c34810801261d222d49e392e1580c34198b9139
Hogan <purmonth@gmail.com>
2025-05-04 19:16:58 -0400

    Time: 11 ms (70.74%), Space: 24.3 MB (81.25%) - LeetHub

---
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        freq = defaultdict(int)
+        for domino in dominoes:
+            freq[tuple(sorted(domino))] += 1
+        
+        result = 0
+        for value in freq.values():
+            result += (value - 1) * value // 2
+            
+        return result
+
+
+dominoes = [[1, 2], [2, 1], [3, 4], [5, 6]]
+print(Solution().numEquivDominoPairs(dominoes))
+dominoes = [[1, 2], [1, 2], [1, 1], [1, 2], [2, 2]]
+print(Solution().numEquivDominoPairs(dominoes))
