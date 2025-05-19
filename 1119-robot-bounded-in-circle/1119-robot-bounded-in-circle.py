2f658c15be3ee7d16c702fdab169a0bdc3a71f99
Hogan <purmonth@gmail.com>
2025-05-19 01:22:08 -0400

    Time: 0 ms (100%), Space: 17.7 MB (88.07%) - LeetHub

---
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        instructions = instructions * 4
+        DIRS = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+        currPosition = [0, 0]
+        currDir = 0
+        for instruction in instructions:
+            if instruction == 'G':
+                currPosition = [currPosition[0] + DIRS[currDir][0], currPosition[1] + DIRS[currDir][1]]
+            if instruction == 'L':
+                currDir = (currDir + 1 + 4) % 4
+            if instruction == 'R':
+                currDir = (currDir - 1 + 4) % 4
+        
+        return currPosition == [0, 0]
+
+
+instructions = "GGLLGG"
+print(Solution().isRobotBounded(instructions))
+instructions = "GG"
+print(Solution().isRobotBounded(instructions))
+instructions = "GL"
+print(Solution().isRobotBounded(instructions))
