e974d493c194cfd0c237ee04a1fbf4facdaee671
Hogan <purmonth@gmail.com>
2025-05-26 11:21:28 +0800

    update readme

--- Python/0852-peak-index-in-a-mountain-array.py
+++ Python/0852-peak-index-in-a-mountain-array.py
@@ -1,3 +1,24 @@
+# time complexity: O(logn)
+# space complexity: O(1)
+from typing import List
+
+
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        return arr.index(max(arr))
+        left = 0
+        right = len(arr) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            elif arr[mid] > arr[mid + 1]:
+                right = mid
+        return left
+
+
+arr = [0, 10, 5, 2]
+print(Solution().peakIndexInMountainArray(arr))
+arr = [0, 2, 1, 0]
+print(Solution().peakIndexInMountainArray(arr))
+arr = [0, 10, 5, 2]
+print(Solution().peakIndexInMountainArray(arr))
