54d5dd0f4332b92ba5f544cf2125def07a136eee
Hogan <purmonth@gmail.com>
2025-05-28 16:13:49 +0800

    Time: 736 ms (10.08%), Space: 33.3 MB (18.03%) - LeetHub

---
+++ 1562-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1562-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
+        subsetCompaniesIdx = set()
+        for i in range(len(favoriteCompanies)):
+            for j in range(len(favoriteCompanies)):
+                if i == j:
+                    continue
+                if set(favoriteCompanies[j]).issubset(set(favoriteCompanies[i])):
+                    subsetCompaniesIdx.add(j)
+        result = [num for num in range(len(favoriteCompanies)) if num not in subsetCompaniesIdx]
+        return result
+
+
+favoriteCompanies = [["leetcode", "google", "facebook"],
+                     ["google", "microsoft"],
+                     ["google", "facebook"],
+                     ["google"],
+                     ["amazon"]]
+print(Solution().peopleIndexes(favoriteCompanies))
+favoriteCompanies = [["leetcode", "google", "facebook"],
+                     ["leetcode", "amazon"], ["facebook", "google"]]
+print(Solution().peopleIndexes(favoriteCompanies))
+favoriteCompanies = [["leetcode"], ["google"], ["facebook"], ["amazon"]]
+print(Solution().peopleIndexes(favoriteCompanies))
