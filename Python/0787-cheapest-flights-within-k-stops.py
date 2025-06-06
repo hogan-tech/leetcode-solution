5a4679b5138faafe10ebc903dacba6a942a78220
Hogan <purmonth@gmail.com>
2025-06-06 14:29:28 +0800

    update readme

--- Python/0787-cheapest-flights-within-k-stops.py
+++ Python/0787-cheapest-flights-within-k-stops.py
@@ -1,7 +1,6 @@
 # time complexity: O(e*k)
 # space complexity: O(n)
-from collections import defaultdict
-from queue import Queue
+from collections import defaultdict, deque
 from typing import List
 
 
@@ -9,33 +8,46 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         adj = defaultdict(list)
         for flight in flights:
-            adj[flight[0]].append((flight[1], flight[2]))
+            start, end, price = flight
+            adj[start].append((end, price))
 
-        dist = [float('inf')] * n
-        dist[src] = 0
+        priceList = [float('inf') for _ in range(n)]
 
-        q = Queue()
-        q.put((src, 0))
+        priceList[src] = 0
+        queue = deque()
+        queue.append((src, 0))
         stops = 0
 
-        while not q.empty() and stops <= k:
-            sz = q.qsize()
-            for _ in range(sz):
-                node, distance = q.get()
-
-                if node not in adj:
-                    continue
-
-                for neighbour, price in adj[node]:
-                    if price + distance >= dist[neighbour]:
-                        continue
-                    dist[neighbour] = price + distance
-                    q.put((neighbour, dist[neighbour]))
-
+        while queue and stops <= k:
+            size = len(queue)
+            for _ in range(size):
+                currNode, currPrice = queue.popleft()
+                for nextNode, nextPrice in adj[currNode]:
+                    if currPrice + nextPrice < priceList[nextNode]:
+                        priceList[nextNode] = currPrice + nextPrice
+                        queue.append((nextNode, currPrice + nextPrice))
             stops += 1
+        
+        return priceList[dst] if priceList[dst] != float('inf') else -1
 
-        return dist[dst] if dist[dst] != float('inf') else -1
+        
 
+# Bellman Ford
+# class Solution:
+#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+#         dist_price = [float('inf') for _ in range(n)]
+#         dist_price[src]=0
+#         for source,dest,cost in flights:
+#             if src==source:
+#                 dist_price[dest] = cost
+#         for times in range(0,K):
+#             temp = [*dist_price]
+#             for srce,dest,cost in flights:
+#                 temp[dest] = min(temp[dest] , cost + dist_price[srce])
+#             dist_price = temp
+#         if dist_price[dst] == float('inf'):
+#             return -1
+#         return dist_price[dst]
 
 n = 4
 flights = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]]
