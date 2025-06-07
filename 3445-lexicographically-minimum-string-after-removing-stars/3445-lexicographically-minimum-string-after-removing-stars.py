37fdaf33c3fc2e66d0653c765d7e7619ed677b92
Hogan <purmonth@gmail.com>
2025-06-07 21:54:06 +0700

    Time: 619 ms (48.2%), Space: 23.5 MB (73.38%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+
+class Solution:
+    def clearStars(self, s: str) -> str:
+        counter = defaultdict(list)
+        arrList = list(s)
+        for i, c in enumerate(arrList):
+            if c != '*':
+                counter[ord(c) - ord('a')].append(i)
+            else:
+                for j in range(26):
+                    if counter[j]:
+                        arrList[counter[j].pop()] = '*'
+                        break
+        return "".join(c for c in arrList if c != '*')
+
+
+s = "aaba*"
+print(Solution().clearStars(s))
+s = "abc"
+print(Solution().clearStars(s))
