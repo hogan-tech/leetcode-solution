e72a0a032c83c99494d74ef5df31ebc9d6820914
Hogan <purmonth@gmail.com>
2025-06-09 10:10:14 +0800

    update readme

--- Python/0763-partition-labels.py
+++ Python/0763-partition-labels.py
@@ -5,16 +5,31 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last = {c: i for i, c in enumerate(s)}
-        j = anchor = 0
-        ans = []
+        lastIdx = {c: i for i, c in enumerate(s)}
+
+        partitionEnd = 0
+        partitionStart = 0
+        partitionSize = []
+
         for i, c in enumerate(s):
-            j = max(j, last[c])
-            if i == j:
-                ans.append(i - anchor + 1)
-                anchor = i + 1
-        return ans
+            partitionEnd = max(partitionEnd, lastIdx[c])
+            if i == partitionEnd:
+                partitionSize.append(partitionEnd - partitionStart + 1)
+                partitionStart = partitionEnd + 1
+
+        return partitionSize
 
 
+'''
+ababcbacadefegdehijhklij
+{'a': 8, 'b': 5, 'c': 7, 'd': 14, 'e': 15, 'f': 11,
+    'g': 13, 'h': 19, 'i': 22, 'j': 23, 'k': 20, 'l': 21}
+
+5 == 5 
+result.append
+
+'''
 s = "ababcbacadefegdehijhklij"
+print(Solution().partitionLabels(s))
+s = "eccbbbbdec"
 print(Solution().partitionLabels(s))
