2650bff390f3c91fcd81d31a89fba599eda439f8
Hogan <purmonth@gmail.com>
2025-06-09 14:38:59 +0800

    update readme

--- Python/0647-palindromic-substrings.py
+++ Python/0647-palindromic-substrings.py
@@ -2,24 +2,21 @@
 # space complexity: O(n^2)
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        count = 0
         n = len(s)
         dp = [[False for _ in range(n)] for _ in range(n)]
-        for i in range(n):
-            dp[i][i] = True
-            count += 1
+
+        for right in range(n):
+            dp[right][right] = True
+            for left in range(right):
+                if s[left] == s[right] and (right - left <= 2 or dp[left + 1][right - 1]):
+                    dp[left][right] = True
         
-        for i in range(n - 1):
-            dp[i][i+1] = (s[i] == s[i+1])
-            count += dp[i][i+1]
-
-        for length in range(3, n + 1):
-            i = 0
-            for j in range(length - 1, n):
-                dp[i][j] = dp[i+1][j-1] and (s[i] == s[j])
-                count +=  dp[i][j]
-                i += 1
-
+        count = 0
+        for i in range(n):
+            for j in range(n):
+                if dp[i][j]:
+                    count += 1
+        
         return count
 
 
