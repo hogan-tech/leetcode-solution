87120f17b35eb3a6956aefda573e4a5bb1d0ca0a
Hogan <purmonth@gmail.com>
2025-06-11 16:02:45 +0800

    update readme

--- Python/0073-set-matrix-zeroes.py
+++ Python/0073-set-matrix-zeroes.py
@@ -16,7 +16,34 @@ class Solution:
             for c in range(len(matrix[0])):
                 if r in rowSet or c in colSet:
                     matrix[r][c] = 0
-            
+
+# time complexity: O(r*c)
+# space complexity: O(1)
+class Solution(object):
+    def setZeroes(self, grid: List[List[int]]) -> None:
+        isCol = False
+        ROW = len(grid)
+        COL = len(grid[0])
+        for r in range(ROW):
+            if grid[r][0] == 0:
+                isCol = True
+            for c in range(1, COL):
+                if grid[r][c] == 0:
+                    grid[0][c] = 0
+                    grid[r][0] = 0
+
+        for r in range(1, ROW):
+            for c in range(1, COL):
+                if not grid[r][0] or not grid[0][c]:
+                    grid[r][c] = 0
+
+        if grid[0][0] == 0:
+            for c in range(COL):
+                grid[0][c] = 0
+
+        if isCol:
+            for r in range(ROW):
+                grid[r][0] = 0
 
 
 matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
