82865ba2bccf8673cc7fb3be027079cfdd729b68
Hogan <purmonth@gmail.com>
2025-06-15 10:11:03 +0800

    update readme

--- Python/0055-jump-game.py
+++ Python/0055-jump-game.py
@@ -5,13 +5,28 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        targetIdx = len(nums) - 1
+        lastPos = len(nums) - 1
         for i in range(len(nums) - 1, -1, -1):
-            if nums[i] + i >= targetIdx:
-                targetIdx = i
-        if targetIdx == 0:
-            return True
-        return False
+            if i + nums[i] >= lastPos:
+                lastPos = i
+        return lastPos == 0
+    
+# time complexity: O(n^2)
+# space complexity: O(n)
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        GOOD = 1
+        BAD = 0
+        UNKNOWN = -1
+        dp = [UNKNOWN for _ in range(len(nums))]
+        dp[-1] = GOOD
+        for i in range(len(nums) - 2, -1, -1):
+            lastPos = min(i + nums[i], len(nums) - 1)
+            for j in range(i + 1, lastPos + 1):
+                if dp[j] == GOOD:
+                    dp[i] = GOOD
+                    break
+        return dp[0] == GOOD
 
 
 nums = [2, 3, 1, 1, 4]
