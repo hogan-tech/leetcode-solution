cce6b2f8a990b019ed610aed85df3a8e271dd2da
Hogan <purmonth@gmail.com>
2025-06-16 14:51:05 +0800

    update readme

--- Python/0198-house-robber.py
+++ Python/0198-house-robber.py
@@ -1,6 +1,19 @@
 # time complexity: O(n)
-# space complexity: O(1)
+# space complexity: O(n)
 from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[n-1]
 
 
 class Solution:
