7b07c1c6c3f1108aa3afa1b7d92ee67838a5cdda
Hogan <purmonth@gmail.com>
2025-06-17 14:20:54 +0800

    update readme

--- Python/1143-longest-common-subsequence.py
+++ Python/1143-longest-common-subsequence.py
@@ -8,13 +8,13 @@ from functools import lru_cache
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dpGrid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
-        for col in reversed(range(len(text2))):
-            for row in reversed(range(len(text1))):
-                if text1[row] == text2[col]:
-                    dpGrid[row][col] = dpGrid[row+1][col+1] + 1
+        for t2 in reversed(range(len(text2))):
+            for t1 in reversed(range(len(text1))):
+                if text1[t1] == text2[t2]:
+                    dpGrid[t1][t2] = dpGrid[t1+1][t2+1] + 1
                 else:
-                    dpGrid[row][col] = max(
-                        dpGrid[row+1][col], dpGrid[row][col+1])
+                    dpGrid[t1][t2] = max(
+                        dpGrid[t1+1][t2], dpGrid[t1][t2+1])
         return dpGrid[0][0]
 
 # time complexity: O(n*m)
@@ -23,21 +23,21 @@ class Solution:
 # Top Down
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        ROW = len(text1)
-        COL = len(text2)
-        dp = [[0 for _ in range(COL + 1)] for _ in range(ROW + 1)]
-        for r in range(ROW):
-            for c in range(COL):
-                if text1[r] == text2[c]:
-                    if r == 0 and c == 0:
-                        dp[r][c] = 1
+        T1 = len(text1)
+        T2 = len(text2)
+        dp = [[0 for _ in range(T2 + 1)] for _ in range(T1 + 1)]
+        for t1 in range(T1):
+            for t2 in range(T2):
+                if text1[t1] == text2[t2]:
+                    if t1 == 0 and t2 == 0:
+                        dp[t1][t2] = 1
                     else:
-                        dp[r][c] = dp[r-1][c-1] + 1
-                if r > 0 and dp[r-1][c] > dp[r][c]:
-                    dp[r][c] = dp[r-1][c]
-                if c > 0 and dp[r][c-1] > dp[r][c]:
-                    dp[r][c] = dp[r][c-1]
-        return dp[ROW - 1][COL - 1]
+                        dp[t1][t2] = dp[t1-1][t2-1] + 1
+                if t1 > 0 and dp[t1-1][t2] > dp[t1][t2]:
+                    dp[t1][t2] = dp[t1-1][t2]
+                if t2 > 0 and dp[t1][t2-1] > dp[t1][t2]:
+                    dp[t1][t2] = dp[t1][t2-1]
+        return dp[T1 - 1][T2 - 1]
 
 
 class Solution:
@@ -55,4 +55,10 @@ class Solution:
 
 text1 = "abcde"
 text2 = "ace"
+print(Solution().longestCommonSubsequence(text1, text2))
+text1 = "abc"
+text2 = "abc"
+print(Solution().longestCommonSubsequence(text1, text2))
+text1 = "abc"
+text2 = "def"
 print(Solution().longestCommonSubsequence(text1, text2))
