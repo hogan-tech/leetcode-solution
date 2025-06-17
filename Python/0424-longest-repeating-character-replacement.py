b0377a5b7bd99e7b5c316f6a18ccb887ffb597b8
Hogan <purmonth@gmail.com>
2025-06-17 12:40:50 +0800

    update readme

--- Python/0424-longest-repeating-character-replacement.py
+++ Python/0424-longest-repeating-character-replacement.py
@@ -6,23 +6,38 @@ from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         result = 0
-        left = right = 0
         freq = defaultdict(int)
-        mostFreqChar = 0
+        left = 0
+        maxFreq = 0
         for right in range(len(s)):
             freq[s[right]] += 1
-
-            mostFreqChar = max(mostFreqChar, freq[s[right]])
-
-            if right - left + 1 - mostFreqChar > k:
+            maxFreq = max(maxFreq, freq[s[right]])
+            if right - left + 1 - maxFreq > k:
                 freq[s[left]] -= 1
                 left += 1
-            
             result = max(result, right - left + 1)
-
         return result
+        
+
+'''
+l
+    r
+AABABBA
+
+(r - l + 1) + maxFreq > k
+l ++
+
+freq = {
+    A: 2
+    B: 1
+}
+
+'''
 
 
+s = "AABABBA"
+k = 1
+print(Solution().characterReplacement(s, k))
 s = "AABABBA"
 k = 1
 print(Solution().characterReplacement(s, k))
