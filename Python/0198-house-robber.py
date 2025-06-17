8bb8e263da66bf8786b455f4cd2c429c0e712b05
Hogan <purmonth@gmail.com>
2025-06-17 16:33:05 +0800

    update readme

--- Python/0198-house-robber.py
+++ Python/0198-house-robber.py
@@ -1,5 +1,6 @@
 # time complexity: O(n)
 # space complexity: O(n)
+from functools import lru_cache
 from typing import List
 
 
@@ -8,34 +9,26 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        dp = [0] * n
+        dp = [0 for _ in range(n)]
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
         for i in range(2, n):
             dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         return dp[n-1]
 
-
+# time complexity: O(n)
+# space complexity: O(1)
 class Solution:
-    def __init__(self) -> None:
-        self.memo = {}
-
     def rob(self, nums: List[int]) -> int:
-        self.memo = {}
-        return self.robFrom(0, nums)
-
-    def robFrom(self, i, nums):
-
-        if i >= len(nums):
-            return 0
-
-        if i in self.memo:
-            return self.memo[i]
-
-        self.memo[i] = max(self.robFrom(i+1, nums),
-                           self.robFrom(i+2, nums) + nums[i])
-        return self.memo[i]
+        @lru_cache(None)
+        def robFrom(i):
+            if i >= len(nums):
+                return 0
+            return max(robFrom(i+1), robFrom(i+2) + nums[i])
+        return robFrom(0)
 
 
 nums = [1, 2, 3, 1]
+print(Solution().rob(nums))
+nums = [2, 7, 9, 3, 1]
 print(Solution().rob(nums))
