81d494eedd6d44c5b5a52b47dabed322b7bd005a
Hogan <purmonth@gmail.com>
2025-06-18 11:02:34 +0800

    update readme

--- Python/0371-sum-of-two-integers.py
+++ Python/0371-sum-of-two-integers.py
@@ -3,24 +3,17 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         x, y = abs(a), abs(b)
-        # ensure that abs(a) >= abs(b)
         if x < y:
             return self.getSum(b, a)
 
-        # abs(a) >= abs(b) -->
-        # a determines the sign
         sign = 1 if a > 0 else -1
 
         if a * b >= 0:
-            # sum of two positive integers x + y
-            # where x > y
             while y:
                 answer = x ^ y
                 carry = (x & y) << 1
                 x, y = answer, carry
         else:
-            # difference of two integers x - y
-            # where x > y
             while y:
                 answer = x ^ y
                 borrow = ((~x) & y) << 1
@@ -28,13 +21,62 @@ class Solution:
 
         return x * sign
 
+'''
+x = 15
+y = 2
 
+0 1 1 1 1
+0 0 0 1 0
+
+x^y = answer
+0 1 1 0 1
+
+(x & y) << 1 = carry
+0 0 1 0 0
+
+if x & y same sign
+1. 
+0 1 1 1 1
+0 0 0 1 0
+2. 
+0 1 1 0 1
+0 0 1 0 0
+3.
+0 1 0 0 1
+0 1 0 0 0
+4.
+0 0 0 0 1
+1 0 0 0 0
+5.
+1 0 0 0 1 -> 16
+0 0 0 0 0
+
+
+if x & y diff sign
+
+x = 15, y = 2, ~x = -16
+
+0 1 1 1 1
+1 0 0 0 0
+0 0 0 1 0
+
+0 1 1 0 1
+'''
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        
+        while b != 0:
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        
+        maxInt = 0x7FFFFFFF
+        return a if a < maxInt else ~(a ^ mask)
+    
 a = 1
 b = 2
 print(Solution().getSum(a, b))
-
-
-velocities = [0, 0.5, 1, 1.5]
-movement = velocities[1:3]
-
-print(movement)
+a = 2
+b = 3
+print(Solution().getSum(a, b))
