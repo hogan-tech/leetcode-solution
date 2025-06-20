79b9c6f2a6f43d6965e9b4c0798e665ae5d4c3c5
Hogan <purmonth@gmail.com>
2025-06-20 15:10:33 +0700

    Time: 0 ms (100%), Space: 19.8 MB (40.8%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,14 +1,20 @@
+# time complexity: O(n)
+# space complexity: O(n)
+from typing import List
+
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        i = 0
         result = []
-        while i < len(intervals) and intervals[i][1] < newInterval[0] :
+        i = 0
+        while i < len(intervals) and newInterval[0] > intervals[i][1]:
             result.append(intervals[i])
             i += 1
-        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+        while i < len(intervals) and newInterval[1] >= intervals[i][0]:
             newInterval[0] = min(intervals[i][0], newInterval[0])
             newInterval[1] = max(intervals[i][1], newInterval[1])
             i += 1
+
         result.append(newInterval)
 
         while i < len(intervals):
@@ -17,12 +23,9 @@ class Solution:
         return result
 
 
-'''
-intervals = [[1,3],[6,9]]
-newInterval = [2,5]
-
-[1, 3]
-
-[2, 5]
-
-'''
+intervals = [[1, 3], [6, 9]]
+newInterval = [2, 5]
+print(Solution().insert(intervals, newInterval))
+intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+newInterval = [4, 8]
+print(Solution().insert(intervals, newInterval))
