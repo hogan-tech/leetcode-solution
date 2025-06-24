2addfa380df474898245ca5efa8dd0f28469cbec
Hogan <purmonth@gmail.com>
2025-06-24 15:16:49 +0800

    update readme

--- Python/0152-maximum-product-subarray.py
+++ Python/0152-maximum-product-subarray.py
@@ -5,23 +5,20 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        maxSoFar = nums[0]
-        minSoFar = nums[0]
-        result = maxSoFar
-        for i in range(1, len(nums)):
-            currNum = nums[i]
-            preMax = maxSoFar
-            maxSoFar = max(currNum, minSoFar * currNum, maxSoFar * currNum)
-            minSoFar = min(currNum, minSoFar * currNum, preMax * currNum)
-            result = max(result, maxSoFar)
+        currMax = 1
+        currMin = 1
+        result = nums[0]
+
+        for num in nums:
+            vals = (num, num * currMin, num * currMax)
+            currMin = min(vals)
+            currMax = max(vals)
+            result = max(result, currMax)
 
         return result
 
 
 nums = [2, 3, -2, 4]
 print(Solution().maxProduct(nums))
-
 nums = [-2, 0, -1]
 print(Solution().maxProduct(nums))
