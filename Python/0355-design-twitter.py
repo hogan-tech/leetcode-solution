004f03eb4e63b0386bdc64e9d8205d51dd03b723
Hogan <purmonth@gmail.com>
2025-06-24 16:43:19 +0800

    update readme

--- Python/0355-design-twitter.py
+++ Python/0355-design-twitter.py
@@ -1,51 +1,66 @@
-from collections import defaultdict, deque
+# time complexity: O(nlogn)
+# space complexity: O(n)
+from collections import defaultdict
 from typing import List
 
 
 class Twitter:
 
     def __init__(self):
-        self.follows = defaultdict(set)
-        self.feed = deque()
+        self.users = defaultdict(list)
+        self.time = 1
+        self.followers = defaultdict(set)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.feed.appendleft((userId, tweetId))
+        self.users[userId].append((self.time, tweetId))
+        self.time += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        return [tweetId for user, tweetId in self.feed if userId == user or user in self.follows[userId]][:10]
+        news = list(self.users[userId])
+        for user in self.followers[userId]:
+            news.extend(self.users[user])
+        news.sort(reverse=True, key=lambda x: x[0])
+        result = []
+        for i in range(len(news)):
+            if i == 10:
+                break
+            result.append(news[i][1])
+        return result
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.follows[followerId].add(followeeId)
+        if followerId != followeeId:
+            self.followers[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        self.follows[followerId].discard(followeeId)
+        if followeeId in self.followers[followerId]:
+            self.followers[followerId].remove(followeeId)
 
 
 # Your Twitter object will be instantiated and called as such:
 # ["Twitter","postTweet","getNewsFeed","follow","getNewsFeed","unfollow","getNewsFeed"]
 # [[],[1,1],[1],[2,1],[2],[2,1],[2]]
-# obj = Twitter()
-# obj.postTweet(1, 1)
-# print(obj.getNewsFeed(1))
-# obj.follow(2, 1)
-# print(obj.getNewsFeed(2))
-# obj.unfollow(2, 1)
-# print(obj.getNewsFeed(2))
+obj1 = Twitter()
+obj1.postTweet(1, 1)
+print(obj1.getNewsFeed(1))
+obj1.follow(2, 1)
+print(obj1.getNewsFeed(2))
+obj1.unfollow(2, 1)
+print(obj1.getNewsFeed(2))
 
 
 # ["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
 # [[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
-# obj = Twitter()
-# obj.postTweet(1, 5)
-# print(obj.getNewsFeed(1))
-# obj.follow(1, 2)
-# obj.postTweet(2, 6)
-# print(obj.getNewsFeed(1))
-# obj.unfollow(1, 2)
-# print(obj.getNewsFeed(1))
+obj2 = Twitter()
+obj2.postTweet(1, 5)
+print(obj2.getNewsFeed(1))
+obj2.follow(1, 2)
+obj2.postTweet(2, 6)
+print(obj2.getNewsFeed(1))
+obj2.unfollow(1, 2)
+print(obj2.getNewsFeed(1))
 
 
 # ["Twitter","getNewsFeed"]
 # [[],[1]]
-obj = Twitter()
-obj.getNewsFeed(1)
+obj3 = Twitter()
+obj3.getNewsFeed(1)
