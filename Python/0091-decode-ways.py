1f3fef332762d55086d53853fd7cdc296974c67a
Hogan <purmonth@gmail.com>
2025-06-24 16:22:11 +0800

    update readme

--- Python/0091-decode-ways.py
+++ Python/0091-decode-ways.py
@@ -4,37 +4,39 @@ from functools import lru_cache
 
 # Top Down
 class Solution:
-    @lru_cache(None)
-    def recursiveWithMemo(self, index: int, s: str) -> int:
-        if index == len(s):
-            return 1
-        if s[index] == '0':
-            return 0
-        if index == len(s) - 1:
-            return 1
-        answer = self.recursiveWithMemo(index + 1, s)
-        if int(s[index: index + 2]) <= 26:
-            answer += self.recursiveWithMemo(index + 2, s)
-        return answer
-
     def numDecodings(self, s: str) -> int:
-        return self.recursiveWithMemo(0, s)
-    
+        
+        @lru_cache(None)
+        def dp(idx: int) -> int:
+            if idx == len(s):
+                return 1
+            if s[idx] == '0':
+                return 0
+            if idx == len(s) - 1:
+                return 1
+            result = dp(idx + 1)
+            if int(s[idx: idx + 2]) <= 26:
+                result += dp(idx + 2)
+            return result
+        
+        return dp(0)
+
+# time complexity: O(n)
+# space complexity: O(n)
 class Solution:
     def numDecodings(self, s: str) -> int:
-        n = len(s)
-        dp = [0] * (n + 1)
-        dp[0] = 1
         if s[0] == '0':
             return 0
-        else:
-            dp[1] = 1
-
+        
+        n = len(s)
+        dp = [0 for _ in range(n + 1)]
+        dp[0] = 1
+        dp[1] = 1
         for i in range(2, n + 1):
             if s[i-1] != '0':
-                dp[i] += dp[i-1]
-            if s[i-2] == '1' or (s[i-2] == '2' and s[i-1] <= '6'):
-                dp[i] += dp[i-2]
+                dp[i] += dp[i - 1]
+            if s[i-2] == '1' or (s[i - 2] == '2' and s[i - 1] <= '6'):
+                dp[i] += dp[i - 2]
 
         return dp[n]
 
