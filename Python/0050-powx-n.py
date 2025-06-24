cd2a3ff71cb65a521448db96f9c2d1c8a3171c99
Hogan <purmonth@gmail.com>
2025-06-24 10:20:02 +0800

    update readme

--- Python/0050-powx-n.py
+++ Python/0050-powx-n.py
@@ -1,28 +1,50 @@
+# time complexity: O(logn)
+# space complexity: O(logn)
+from functools import lru_cache
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        @lru_cache(None)
+        def binaryExp(x, n):
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+            if n < 0:
+                return 1.0 / binaryExp(x, -n)
+            if n % 2:
+                return x * binaryExp(x * x, (n - 1) // 2)
+            else:
+                return binaryExp(x * x, n // 2)
+        return binaryExp(x, n)
+
+# time complexity: O(logn)
+# space complexity: O(1)
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
         if n < 0:
-            if abs(x) < 1e-6:
-                raise ValueError("Cannot calculate power for x close to zero with negative n")
-            result = 1.0
-            x = 1 / x
-            n = -n
-            while n > 0:
-                if n % 2 == 1:
-                    result *= x
-                x *= x
-                n //= 2
-            return result
-        else:
-            result = 1.0
-            while n > 0:
-                if n % 2 == 1:
-                    result *= x
-                x *= x
-                n //= 2
-            return result
+            n = -1 * n
+            x = 1.0 / x
+
+        result = 1
+        while n != 0:
+            if n % 2 == 1:
+                result *= x
+                n -= 1
+            x *= x
+            n //= 2
+        return result
 
 
 x = 2.00000
-n = -10
+n = 10
+print(Solution().myPow(x, n))
+x = 2.10000
+n = 3
+print(Solution().myPow(x, n))
+x = 2.00000
+n = -2
+print(Solution().myPow(x, n))
--- README.md
+++ README.md
@@ -84,15 +84,3 @@ We welcome contributions! If you have a solution to a problem that isn't already
 ## License
 
 This project is licensed under the MIT License. See the [LICENSE](./LICENSE) file for details.
-
-<!---LeetCode Topics Start-->
-# LeetCode Topics
-## Math
-|  |
-| ------- |
-| [0050-powx-n](https://github.com/hogan-tech/leetcode-solution/tree/master/0050-powx-n) |
-## Recursion
-|  |
-| ------- |
-| [0050-powx-n](https://github.com/hogan-tech/leetcode-solution/tree/master/0050-powx-n) |
-<!---LeetCode Topics End-->
