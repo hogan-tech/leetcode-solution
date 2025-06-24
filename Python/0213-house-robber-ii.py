8483ce488f3db9c66ce8a639f5c21f340aaeb348
Hogan <purmonth@gmail.com>
2025-06-24 14:06:26 +0800

    update readme

--- Python/0213-house-robber-ii.py
+++ Python/0213-house-robber-ii.py
@@ -1,5 +1,6 @@
 # time complexity: O(n)
 # space complexity: O(n)
+from functools import lru_cache
 from typing import List
 
 
@@ -20,6 +21,25 @@ class Solution:
             return nums[0]
         return max(robMoney(nums[:-1]), robMoney(nums[1:]))
 
+# time complexity: O(n)
+# space complexity: O(n)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def robMoney(start: int, end: int) -> int:
+            @lru_cache(None)
+            def dp(i):
+                if i > end:
+                    return 0
+                return max(nums[i] + dp(i + 2), dp(i + 1))
+            return dp(start)
+
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        return max(robMoney(0, n - 2), robMoney(1, n - 1))
 
 nums = [2, 3, 2]
 print(Solution().rob(nums))
