b9ee3c98f4c90036eb76df97f3987d03eb40d4f7
Hogan <purmonth@gmail.com>
2025-07-13 09:11:02 +0700

    Time: 0 ms (100%), Space: 17.9 MB (73.53%) - LeetHub

--- 2028-the-earliest-and-latest-rounds-where-players-compete/2028-the-earliest-and-latest-rounds-where-players-compete.py
+++ 2028-the-earliest-and-latest-rounds-where-players-compete/2028-the-earliest-and-latest-rounds-where-players-compete.py
@@ -1,43 +1,52 @@
+# time complexity: O(n^4 * logn)
+# space complexity: O(n^3)
+from functools import lru_cache
+from typing import List
+
+
 class Solution:
-    def earliestAndLatest(
-        self, n: int, firstPlayer: int, secondPlayer: int
-    ) -> List[int]:
-        @cache
-        def dp(n: int, f: int, s: int) -> (int, int):
+    def earliestAndLatest(self, n: int, firstPlayer: int, secondPlayer: int) -> List[int]:
+
+        @lru_cache(None)
+        def dp(n: int, f: int, s: int):
             if f + s == n + 1:
                 return (1, 1)
 
-            # F(n,f,s)=F(n,n+1-s,n+1-f)
             if f + s > n + 1:
                 return dp(n, n + 1 - s, n + 1 - f)
 
             earliest, latest = float("inf"), float("-inf")
-            n_half = (n + 1) // 2
+            nHalf = (n + 1) // 2
 
-            if s <= n_half:
-                # s On the left or in the middle
+            if s <= nHalf:
                 for i in range(f):
                     for j in range(s - f):
-                        x, y = dp(n_half, i + 1, i + j + 2)
+                        x, y = dp(nHalf, i + 1, i + j + 2)
                         earliest = min(earliest, x)
                         latest = max(latest, y)
             else:
-                # s on the right
-                # s'
-                s_prime = n + 1 - s
-                mid = (n - 2 * s_prime + 1) // 2
+                sPrime = n + 1 - s
+                mid = (n - 2 * sPrime + 1) // 2
                 for i in range(f):
-                    for j in range(s_prime - f):
-                        x, y = dp(n_half, i + 1, i + j + mid + 2)
+                    for j in range(sPrime - f):
+                        x, y = dp(nHalf, i + 1, i + j + mid + 2)
                         earliest = min(earliest, x)
                         latest = max(latest, y)
 
             return (earliest + 1, latest + 1)
 
-        # F(n,f,s) = F(n,s,f)
         if firstPlayer > secondPlayer:
             firstPlayer, secondPlayer = secondPlayer, firstPlayer
 
         earliest, latest = dp(n, firstPlayer, secondPlayer)
-        dp.cache_clear()
         return [earliest, latest]
+
+
+n = 11
+firstPlayer = 2
+secondPlayer = 4
+print(Solution().earliestAndLatest(n, firstPlayer, secondPlayer))
+n = 5
+firstPlayer = 1
+secondPlayer = 5
+print(Solution().earliestAndLatest(n, firstPlayer, secondPlayer))
