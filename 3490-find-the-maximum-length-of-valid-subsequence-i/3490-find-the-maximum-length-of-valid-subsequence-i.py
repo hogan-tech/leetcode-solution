186f8afbfb3e6a5c577f4f950198988054bf52f6
Hogan <purmonth@gmail.com>
2025-07-16 09:33:30 +0700

    Time: 63 ms (61.24%), Space: 39.6 MB (21.71%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        oddEvenList = [num % 2 for num in nums]
+        oddCount = oddEvenList.count(1)
+        evenCount = oddEvenList.count(0)
+        oddFirstCount = 0
+        oddFirstFlag = 1
+        evenFirstCount = 0
+        evenFirstFlag = 0
+        for num in oddEvenList:
+            if num == oddFirstFlag:
+                oddFirstCount += 1
+                oddFirstFlag = (oddFirstCount + 1) % 2
+            if num == evenFirstFlag:
+                evenFirstCount += 1
+                evenFirstFlag = (evenFirstFlag + 1) % 2
+                
+        return max(oddCount, evenCount, oddFirstCount, evenFirstCount)
+
+
+'''
+1 0 1 0
+
+1 0 1 1 0 1 0
+
+1 1
+'''
+nums = [1, 2, 3, 4]
+print(Solution().maximumLength(nums))
+nums = [1, 2, 1, 1, 2, 1, 2]
+print(Solution().maximumLength(nums))
+nums = [1, 3]
+print(Solution().maximumLength(nums))
