be7f0c4de9c12496cde45434c281c48b63cdd685
Hogan <purmonth@gmail.com>
2025-08-06 10:01:36 +0800

    Time: 159 ms (18.95%), Space: 17.8 MB (73.98%) - LeetHub

---
+++ 3952-trionic-array-i/3952-trionic-array-i.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for p in range(1, n - 2):
+            for q in range(p + 1, n - 1):
+                if not all(nums[i] < nums[i+1] for i in range(0, p)):
+                    continue
+                if not all(nums[i] > nums[i+1] for i in range(p, q)):
+                    continue
+                if not all(nums[i] < nums[i+1] for i in range(q, n - 1)):
+                    continue
+                return True
+        return False
+
+
+nums = [1, 3, 5, 4, 2, 6]
+print(Solution().isTrionic(nums))
+nums = [2, 1, 3]
+print(Solution().isTrionic(nums))
