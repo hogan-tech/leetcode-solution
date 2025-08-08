a7738cf39285a5e7a1b9686e8d795c07458db1c2
Hogan <purmonth@gmail.com>
2025-08-08 08:10:20 +0800

    Time: 134 ms (9.57%), Space: 49.8 MB (5.61%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.py
+++ 0826-soup-servings/0826-soup-servings.py
@@ -1,3 +1,5 @@
+# time complexity: O(1)
+# space complexity: O(1)
 from functools import lru_cache
 from math import ceil
 
@@ -7,17 +9,23 @@ class Solution:
         m = ceil(n/25)
 
         @lru_cache(None)
-        def calculate_dp(i: int, j: int) -> float:
+        def dp(i: int, j: int) -> float:
             if i <= 0 and j <= 0:
                 return 0.5
             if i <= 0:
                 return 1.0
             if j <= 0:
                 return 0.0
-            return (calculate_dp(i-4, j) + calculate_dp(i-3, j-1) +
-                    calculate_dp(i-2, j-2) + calculate_dp(i-1, j-3)) / 4.0
+            return (dp(i-4, j) + dp(i-3, j-1) +
+                    dp(i-2, j-2) + dp(i-1, j-3)) / 4.0
 
         for k in range(1, m + 1):
-            if calculate_dp(k, k) > 1 - 1e-5:
+            if dp(k, k) > 1 - 1e-5:
                 return 1.0
-        return calculate_dp(m, m)
+        return dp(m, m)
+
+
+n = 50
+print(Solution().soupServings(n))
+n = 100
+print(Solution().soupServings(n))
