1c022449d84e6a08c8adc8f03ecc0ad7bda43b30
Hogan <purmonth@gmail.com>
2025-09-05 02:20:34 +0800

    Time: 139 ms (69.04%), Space: 80.1 MB (44.36%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -0,0 +1,56 @@
+# time complexity: O(1)
+# space complexity: O(n)
+from collections import OrderedDict, defaultdict
+
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.key2val = {}
+        self.key2freq = {}
+        self.freq2key = defaultdict(OrderedDict)
+        self.minf = 0
+
+    def get(self, key: int) -> int:
+        if key not in self.key2val:
+            return -1
+        oldfreq = self.key2freq[key]
+        self.key2freq[key] = oldfreq + 1
+        self.freq2key[oldfreq].pop(key)
+        if not self.freq2key[oldfreq]:
+            del self.freq2key[oldfreq]
+        self.freq2key[oldfreq + 1][key] = 1
+        if self.minf not in self.freq2key:
+            self.minf += 1
+        return self.key2val[key]
+
+    def put(self, key: int, value: int) -> None:
+        if self.cap <= 0:
+            return
+        if key in self.key2val:
+            self.get(key)
+            self.key2val[key] = value
+            return
+
+        if len(self.key2val) == self.cap:
+            delkey, _ = self.freq2key[self.minf].popitem(last=False)
+            del self.key2val[delkey]
+            del self.key2freq[delkey]
+        self.key2val[key] = value
+        self.key2freq[key] = 1
+        self.freq2key[1][key] = 1
+        self.minf = 1
+
+
+lfu = LFUCache(2)
+lfu.put(1, 1)   
+lfu.put(2, 2)   
+print(lfu.get(1))      
+lfu.put(3, 3)   
+print(lfu.get(2))      
+print(lfu.get(3))      
+lfu.put(4, 4)   
+print(lfu.get(1))      
+print(lfu.get(3))      
+print(lfu.get(4))      
