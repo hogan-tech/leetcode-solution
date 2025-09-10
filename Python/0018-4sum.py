b42cb84bbb5102815a8974b986bd97cb6684a4a2
Hogan <purmonth@gmail.com>
2025-09-10 19:35:53 -0400

    update readme

--- Python/0018-4sum.py
+++ Python/0018-4sum.py
@@ -7,26 +7,19 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 
         def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
-            res = []
+            result = []
             if not nums:
-                return res
-
-            averageValue = target // k
-            if averageValue < nums[0] or averageValue > nums[-1]:
-                return res
-
+                return result
             if k == 2:
                 return twoSum(nums, target)
-
             for i in range(len(nums)):
                 if i == 0 or nums[i-1] != nums[i]:
                     for subset in kSum(nums[i+1:], target-nums[i], k-1):
-                        res.append([nums[i]]+subset)
-
-            return res
+                        result.append([nums[i]] + subset)
+            return result
 
         def twoSum(nums: List[int], target: int) -> List[List[int]]:
-            res = []
+            result = []
             left, right = 0, len(nums) - 1
             while (left < right):
                 currSum = nums[left] + nums[right]
@@ -35,15 +28,29 @@ class Solution:
                 elif currSum > target or (right < len(nums)-1 and nums[right] == nums[right + 1]):
                     right -= 1
                 else:
-                    res.append([nums[left], nums[right]])
+                    result.append([nums[left], nums[right]])
                     left += 1
                     right -= 1
-            return res
+            return result
+        
+        def twoSum(nums: List[int], target: int) -> List[List[int]]:
+            result = []
+            seen = {}
+            for i, num in enumerate(nums):
+                complement = target - num
+                if complement in seen and (not result or [complement, num] != result[-1]):
+                    result.append([complement, num])
+                seen[num] = i
+            return result
         nums.sort()
         return kSum(nums, target, 4)
+    
+        
 
 
 nums = [1, 0, -1, 0, -2, 2]
 target = 0
-
+print(Solution().fourSum(nums, target))
+nums = [2, 2, 2, 2, 2]
+target = 8
 print(Solution().fourSum(nums, target))
