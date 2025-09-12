574b312e623ff43c31909fced95ae9dfd63267d7
Hogan <purmonth@gmail.com>
2025-09-11 23:11:01 -0400

    update readme

--- Python/0110-balanced-binary-tree.py
+++ Python/0110-balanced-binary-tree.py
@@ -1,4 +1,5 @@
-# Definition for a binary tree node.
+# time complexity: O(nlogn)
+# space complexity: O(n)
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -18,3 +19,30 @@ class Solution(object):
         if abs(self.height(node.left)-self.height(node.right)) > 1:
             return False
         return self.isBalanced(node.left) and self.isBalanced(node.right)
+
+# time complexity: O(n)
+# space complexity: O(n)
+class Solution:
+    def isBalancedHelper(self, root: TreeNode):
+        if not root:
+            return True, -1
+
+        leftIsBalanced, leftHeight = self.isBalancedHelper(root.left)
+        if not leftIsBalanced:
+            return False, 0
+        rightIsBalanced, rightHeight = self.isBalancedHelper(root.right)
+        if not rightIsBalanced:
+            return False, 0
+
+        return (abs(leftHeight - rightHeight) < 2), 1 + max(leftHeight, rightHeight)
+
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.isBalancedHelper(root)[0]
+
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(Solution().isBalanced(root))
