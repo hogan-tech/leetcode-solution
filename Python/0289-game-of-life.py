6a0e776196fd1b6a71c6c38a6bb58fe8f460f7aa
Hogan <purmonth@gmail.com>
2025-09-14 15:32:28 -0400

    update readme

--- Python/0289-game-of-life.py
+++ Python/0289-game-of-life.py
@@ -5,25 +5,24 @@ from typing import List
 
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
-        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1),
-                     (-1, 0), (-1, 1), (0, 1), (1, 1)]
-        rows = len(board)
-        cols = len(board[0])
-        copyBoard = [[board[row][col]
-                      for col in range(cols)] for row in range(rows)]
-        for row in range(rows):
-            for col in range(cols):
+        ROW = len(board)
+        COL = len(board[0])
+        copyBoard = [[board[r][c]for c in range(COL)] for r in range(ROW)]
+        for r in range(ROW):
+            for c in range(COL):
                 liveNeighbors = 0
-                for neighbor in neighbors:
-                    r = (row + neighbor[0])
-                    c = (col + neighbor[1])
-                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (copyBoard[r][c] == 1):
+                for dR, dC in [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]:
+                    nextR = r + dR
+                    nextC = c + dC
+                    if 0 <= nextR < ROW and 0 <= nextC < COL and copyBoard[nextR][nextC] == 1:
                         liveNeighbors += 1
-                if copyBoard[row][col] == 1 and (liveNeighbors < 2 or liveNeighbors > 3):
-                    board[row][col] = 0
-                if copyBoard[row][col] == 0 and liveNeighbors == 3:
-                    board[row][col] = 1
+                if copyBoard[r][c] == 1 and (liveNeighbors < 2 or liveNeighbors > 3):
+                    board[r][c] = 0
+                if copyBoard[r][c] == 0 and liveNeighbors == 3:
+                    board[r][c] = 1
 
 
 board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+print(Solution().gameOfLife(board))
+board = [[1, 1], [1, 0]]
 print(Solution().gameOfLife(board))
