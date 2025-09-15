1e524a2780c5f447df2ecd7188bacbb6721d31be
Hogan <purmonth@gmail.com>
2025-09-15 19:58:27 -0400

    update readme

--- Python/0049-group-anagrams.py
+++ Python/0049-group-anagrams.py
@@ -1,16 +1,21 @@
-# time complexity: O(nklogk)
-# space complexity: O(nk)
+# time complexity: O(nlogn * n)
+# space complexity: O(n)
 from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = defaultdict(list)
-        for s in strs:
-            ans[tuple(sorted(s))].append(s)
-        return ans.values()
+        wordMap = defaultdict(list)
+        for word in strs:
+            key = ''.join(sorted(word))
+            wordMap[key].append(word)
+        return [row for row in wordMap.values()]
 
 
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(Solution().groupAnagrams(strs))
+strs = [""]
+print(Solution().groupAnagrams(strs))
+strs = ["a"]
 print(Solution().groupAnagrams(strs))
