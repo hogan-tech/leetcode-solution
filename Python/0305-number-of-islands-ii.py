63717717213552d24b92de63042de0cb9817e9ee
Hogan <purmonth@gmail.com>
2025-09-17 14:51:32 -0400

    update readme

--- Python/0305-number-of-islands-ii.py
+++ Python/0305-number-of-islands-ii.py
@@ -6,54 +6,54 @@ from typing import List
 class Solution:
     class UnionFind:
         def __init__(self, size):
-            self.rep = [i for i in range(size)]
-            self.rank = [0] * size
+            self.parent = [i for i in range(size)]
+            self.rank = [0 for _ in range(size)]
             self.count = 0
 
         def find(self, node):
-            if node != self.rep[node]:
-                self.rep[node] = self.find(self.rep[node])
-            return self.rep[node]
+            if node != self.parent[node]:
+                self.parent[node] = self.find(self.parent[node])
+            return self.parent[node]
 
         def union(self, node1, node2):
-            rep1 = self.find(node1)
-            rep2 = self.find(node2)
-            if rep1 != rep2:
-                if self.rank[rep1] > self.rank[rep2]:
-                    self.rep[rep2] = rep1
-                elif self.rank[rep2] < self.rank[rep1]:
-                    self.rep[rep1] = rep2
+            parent1 = self.find(node1)
+            parent2 = self.find(node2)
+            if parent1 != parent2:
+                if self.rank[parent1] > self.rank[parent2]:
+                    self.parent[parent2] = parent1
+                elif self.rank[parent2] < self.rank[parent1]:
+                    self.parent[parent1] = parent2
                 else:
-                    self.rep[rep2] = rep1
-                    self.rank[rep1] += 1
+                    self.parent[parent2] = parent1
+                    self.rank[parent1] += 1
                 self.count -= 1
 
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        uf = self.UnionFind(m * n)
-        res = []
-        land_map = set()
-
-        for row, col in positions:
-            if (row, col) in land_map:
-                res.append(uf.count)
+        unionFind = self.UnionFind(m * n)
+        result = []
+        landMap = set()
+        ROW = m
+        COL = n
+        for r, c in positions:
+            if (r, c) in landMap:
+                result.append(unionFind.count)
                 continue
 
-            land_map.add((row, col))
-            uf.count += 1
-
-            for i, j in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                r, c = row + i, col + j
-                if r < 0 or c < 0 or r >= m or c >= n:
-                    continue
-                if (r, c) in land_map:
-                    uf.union(row * n + col, r * n + c)
-
-            res.append(uf.count)
-
-        return res
+            landMap.add((r, c))
+            unionFind.count += 1
+            for dR, dC in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                nextR, nextC = r + dR, c + dC
+                if 0 <= nextR < ROW and 0 <= nextC < COL and (nextR, nextC) in landMap:
+                    unionFind.union(r * COL + c, nextR * COL + nextC)
+            result.append(unionFind.count)
+        return result
 
 
 m = 3
 n = 3
 positions = [[0, 0], [0, 1], [1, 2], [2, 1]]
+print(Solution().numIslands2(m, n, positions))
+m = 1
+n = 1
+positions = [[0, 0]]
 print(Solution().numIslands2(m, n, positions))
--- Readme/0305-number-of-islands-ii.md
+++ Readme/0305-number-of-islands-ii.md
@@ -1,4 +1,4 @@
-<h2><a href="https://leetcode.com/problems/number-of-islands-ii/">305. Number of Islands II</a></h2><h3>Hard</h3><hr><div><p>You are given an empty 2D binary grid <code>grid</code> of size <code>m x n</code>. The grid represents a map where <code>0</code>'s represent water and <code>1</code>'s represent land. Initially, all the cells of <code>grid</code> are water cells (i.e., all the cells are <code>0</code>'s).</p>
+<h2><a href="https://leetcode.com/problems/number-of-islands-ii">305. Number of Islands II</a></h2><h3>Hard</h3><hr><p>You are given an empty 2D binary grid <code>grid</code> of size <code>m x n</code>. The grid represents a map where <code>0</code>&#39;s represent water and <code>1</code>&#39;s represent land. Initially, all the cells of <code>grid</code> are water cells (i.e., all the cells are <code>0</code>&#39;s).</p>
 
 <p>We may perform an add land operation which turns the water at position into a land. You are given an array <code>positions</code> where <code>positions[i] = [r<sub>i</sub>, c<sub>i</sub>]</code> is the position <code>(r<sub>i</sub>, c<sub>i</sub>)</code> at which we should operate the <code>i<sup>th</sup></code> operation.</p>
 
@@ -8,8 +8,9 @@
 
 <p>&nbsp;</p>
 <p><strong class="example">Example 1:</strong></p>
-<img alt="" src="https://assets.leetcode.com/uploads/2021/03/10/tmp-grid.jpg" style="width: 500px; height: 294px;">
-<pre><strong>Input:</strong> m = 3, n = 3, positions = [[0,0],[0,1],[1,2],[2,1]]
+<img alt="" src="https://assets.leetcode.com/uploads/2021/03/10/tmp-grid.jpg" style="width: 500px; height: 294px;" />
+<pre>
+<strong>Input:</strong> m = 3, n = 3, positions = [[0,0],[0,1],[1,2],[2,1]]
 <strong>Output:</strong> [1,1,2,3]
 <strong>Explanation:</strong>
 Initially, the 2d grid is filled with water.
@@ -21,7 +22,8 @@ Initially, the 2d grid is filled with water.
 
 <p><strong class="example">Example 2:</strong></p>
 
-<pre><strong>Input:</strong> m = 1, n = 1, positions = [[0,0]]
+<pre>
+<strong>Input:</strong> m = 1, n = 1, positions = [[0,0]]
 <strong>Output:</strong> [1]
 </pre>
 
@@ -38,4 +40,3 @@ Initially, the 2d grid is filled with water.
 
 <p>&nbsp;</p>
 <p><strong>Follow up:</strong> Could you solve it in time complexity <code>O(k log(mn))</code>, where <code>k == positions.length</code>?</p>
-</div>
