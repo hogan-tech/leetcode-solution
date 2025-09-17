96b9ef2f2b8c94ac605319233627722fd857219b
Hogan <purmonth@gmail.com>
2025-09-17 19:34:30 -0400

    update readme

--- Python/0121-best-time-to-buy-and-sell-stock.py
+++ Python/0121-best-time-to-buy-and-sell-stock.py
@@ -5,17 +5,15 @@ from typing import List
 
 class Solution(object):
     def maxProfit(self, prices: List[int]) -> int:
-        profit, smallestPrice = 0, 99999
+        profit = 0
+        smallestPirce = float("inf")
         for price in prices:
-            if price < smallestPrice:
-                smallestPrice = price
-            else:
-                profit = max(profit, price - smallestPrice)
+            smallestPirce = min(smallestPirce, price)
+            profit = max(profit, price - smallestPirce)
         return profit
 
 
-PriceList = [7, 1, 5, 3, 6, 4]
-
-solution = Solution()
-
-print(solution.maxProfit(PriceList))
+prices = [7, 1, 5, 3, 6, 4]
+print(Solution().maxProfit(prices))
+prices = [7, 6, 4, 3, 1]
+print(Solution().maxProfit(prices))
