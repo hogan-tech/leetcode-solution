43a82cb68fa4de0fe49631aecfcf0f05d43bd510
Hogan <purmonth@gmail.com>
2025-09-19 01:28:46 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        lastPos = len(nums) - 1
+        for i in range(len(nums) -1, -1, -1):
+            if i + nums[i] == lastPos:
+                lastPos = i
+        return lastPos == 0
