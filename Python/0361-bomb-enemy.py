c4da851424ba8da61c00552c392b40f4a21d7001
Hogan <purmonth@gmail.com>
2025-09-18 15:30:43 -0400

    update readme

--- Python/0361-bomb-enemy.py
+++ Python/0361-bomb-enemy.py
@@ -5,37 +5,30 @@ from typing import List
 
 class Solution:
     def maxKilledEnemies(self, grid: List[List[str]]) -> int:
-        if len(grid) == 0:
-            return 0
-
-        ROW, COL = len(grid), len(grid[0])
-
+        ROW = len(grid)
+        COL = len(grid[0])
         maxCount = 0
         rowHits = 0
-        colHits = [0] * COL
-
+        colHits = [0 for _ in range(COL)]
         for r in range(ROW):
             for c in range(COL):
                 if c == 0 or grid[r][c - 1] == 'W':
                     rowHits = 0
-                    for k in range(c, COL):
-                        if grid[r][k] == 'W':
+                    for i in range(c, COL):
+                        if grid[r][i] == 'W':
                             break
-                        elif grid[r][k] == 'E':
+                        elif grid[r][i] == 'E':
                             rowHits += 1
-
                 if r == 0 or grid[r - 1][c] == 'W':
                     colHits[c] = 0
-                    for k in range(r, ROW):
-                        if grid[k][c] == 'W':
+                    for i in range(r, ROW):
+                        if grid[i][c] == 'W':
                             break
-                        elif grid[k][c] == 'E':
+                        elif grid[i][c] == 'E':
                             colHits[c] += 1
-
                 if grid[r][c] == '0':
                     totalHits = rowHits + colHits[c]
                     maxCount = max(maxCount, totalHits)
-
         return maxCount
 
 
