0dcbd85105c8e0fe2f6d045b50d3905965f42cc9
Hogan <purmonth@gmail.com>
2025-09-20 08:20:27 +0800

    Time: 0 ms (100%), Space: 17.7 MB (58.87%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,30 @@
+# time complexity: O(n+m)
+# space complexity: O(n)
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        patternMap, wordMap = {}, {}
+        words = s.split(" ")
+        if len(words) != len(pattern):
+            return False
+        for char, word in zip(pattern, words):
+            if char not in patternMap:
+                if word in wordMap:
+                    return False
+                else:
+                    patternMap[char] = word
+                    wordMap[word] = char
+            else:
+                if patternMap[char] != word:
+                    return False
+        return True
+
+
+pattern = "abba"
+s = "dog cat cat dog"
+print(Solution().wordPattern(pattern, s))
+pattern = "abba"
+s = "dog cat cat fish"
+print(Solution().wordPattern(pattern, s))
+pattern = "aaaa"
+s = "dog cat cat dog"
+print(Solution().wordPattern(pattern, s))
