0f4594d00374410d33eef8051829c20d0b4f8921
Hogan <purmonth@gmail.com>
2025-09-22 03:48:19 +0800

    Time: 0 ms (100%), Space: 17.9 MB (8.37%) - LeetHub

---
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -0,0 +1,26 @@
+# time complexity: O(n)
+# space complexity: O(1)
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+    
+    
+root = ListNode(1)
+root.next = ListNode(2)
+root.next.next = ListNode(3)
+root.next.next.next = ListNode(4)
+root.next.next.next.next = ListNode(5)
+print(Solution().middleNode(root))
