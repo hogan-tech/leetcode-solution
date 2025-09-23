80fa27459248d27a39a3b314496b82c01a07cb27
Hogan <purmonth@gmail.com>
2025-09-24 03:33:42 +0800

    Time: 29 ms (66.37%), Space: 39.7 MB (13.98%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,78 @@
+# time complexity: O(n)
+# space complexity: O(1)
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if head is None:
+            return True
+
+        firstHalfEnd = self.endOfFirstHalf(head)
+        secondHalfStart = self.reverseList(firstHalfEnd.next)
+
+        firstPos = head
+        seconfPos = secondHalfStart
+        while seconfPos:
+            if firstPos.val != seconfPos.val:
+                return False
+            firstPos = firstPos.next
+            seconfPos = seconfPos.next
+            
+        return True
+
+    def endOfFirstHalf(self, head: ListNode) -> ListNode:
+        fast = head
+        slow = head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        return prev
+
+# time complexity: O(n)
+# space complexity: O(n)
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        palindromList = []
+        while head:
+            palindromList.append(head.val)
+            head = head.next
+        i = 0
+        j = len(palindromList) - 1
+        while i < j:
+            if palindromList[i] != palindromList[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+
+
+root = ListNode(1)
+root.next = ListNode(2)
+root.next.next = ListNode(2)
+root.next.next.next = ListNode(1)
+print(Solution().isPalindrome(root))
