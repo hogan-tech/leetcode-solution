1eac73d8e12e3562b2409589256fb7835a812190
Hogan <purmonth@gmail.com>
2025-10-02 06:36:16 +0800

    Time: 0 ms (100%), Space: 18.7 MB (44.97%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,15 @@
+# time complexity: O(logn)
+# space complexity: O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
