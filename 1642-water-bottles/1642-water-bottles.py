27cd7900b775a5403cb43319608c0b200ee99f80
Hogan <purmonth@gmail.com>
2025-10-01 10:09:12 +0800

    Time: 0 ms (100%), Space: 18 MB (17.52%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.py
@@ -0,0 +1,15 @@
+# time complexity: O(1)
+# space complexity: O(1)
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles
+        while numBottles // numExchange >= 1:
+            remains = numBottles % numExchange
+            total += numBottles // numExchange
+            numBottles = numBottles // numExchange + remains
+        return total
+
+
+numBottles = 9
+numExchange = 3
+print(Solution().numWaterBottles(numBottles, numExchange))
