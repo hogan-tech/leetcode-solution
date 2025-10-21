d0f120a9b357067a9736fb3527ab5e1bf8d83ddf
Hogan <purmonth@gmail.com>
2025-10-21 19:43:34 -0400

    Time: 7 ms (38.93%), Space: 18 MB (13.36%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,5 +1,5 @@
 # time complexity: O(n)
-# space complexity: O(1)
+# space complexity: O(k)
 from typing import List
 
 
@@ -14,6 +14,30 @@ class Solution:
             if i == right:
                 result.append(right - left + 1)
                 left = right + 1
+        return result
+
+# time complexity: O(n)
+# space complexity: O(k)
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        result = []
+        lastIdx = [0] * 26
+        firstIdx = [-1] * 26
+        left, right = 0, 0
+        for i, char in enumerate(s):
+            lastIdx[ord(char) - ord("a")] = i
+        for i, char in enumerate(s):
+            index = ord(char) - ord("a")
+            if firstIdx[index] == -1:
+                firstIdx[index] = i
+            if right < firstIdx[index]:
+                result.append(right - left + 1)
+                left = i
+                right = i
+            right = max(right, lastIdx[index])
+        if right - left + 1 > 0:
+            result.append(right - left + 1)
+
         return result
 
 
