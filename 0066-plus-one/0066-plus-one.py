4167138c66d2a7be2936bd40212b601f733bc794
Hogan <purmonth@gmail.com>
2026-01-02 11:53:06 +0800

    update readme

--- 0066-plus-one/0066-plus-one.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        numStr = ''.join([str(digit) for digit in digits])
-        numStr = str(int(numStr) + 1)
-        return [int(digit) for digit in numStr]
-        
-        
