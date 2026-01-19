0bf4ca27674e57866bdb3a85fe8e835837dc31ef
Hogan <purmonth@gmail.com>
2026-01-19 00:42:11 -0500

    Time: 324 ms (39.29%), Space: 26 MB (35.71%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -0,0 +1,44 @@
+# time complexity: O(mnlogmin(m,n))
+# space complexity: O(mn)
+from typing import List
+
+
+class Solution:
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        m, n = len(mat), len(mat[0])
+        P = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                P[i][j] = (
+                    P[i - 1][j]
+                    + P[i][j - 1]
+                    - P[i - 1][j - 1]
+                    + mat[i - 1][j - 1]
+                )
+
+        def getRect(x1, y1, x2, y2):
+            return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1]
+
+        l, r, result = 1, min(m, n), 0
+        while l <= r:
+            mid = (l + r) // 2
+            find = any(
+                getRect(i, j, i + mid - 1, j + mid - 1) <= threshold
+                for i in range(1, m - mid + 2)
+                for j in range(1, n - mid + 2)
+            )
+            if find:
+                result = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return result
+
+
+mat = [[1, 1, 3, 2, 4, 3, 2], [1, 1, 3, 2, 4, 3, 2], [1, 1, 3, 2, 4, 3, 2]]
+threshold = 4
+print(Solution().maxSideLength(mat, threshold))
+mat = [[2, 2, 2, 2, 2], [2, 2, 2, 2, 2], [
+    2, 2, 2, 2, 2], [2, 2, 2, 2, 2], [2, 2, 2, 2, 2]]
+threshold = 1
+print(Solution().maxSideLength(mat, threshold))
